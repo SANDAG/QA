9badec809169ab4dca176dd6bea0cea5fa52c675
Anne Kuller <aku@sandag.org>
2018-06-22 14:13:43 -0700

    plot change in unit type by year instead of total units each year

--- SR14Forecast/SubRegional/HouseHold Unit Type.R
+++ SR14Forecast/SubRegional/HouseHold Unit Type.R
@@ -114,7 +114,7 @@ jur_list2<- c("Carlsbad","Chula Vista","Coronado","Del Mar","El Cajon","Encinita
 
 for(i in 1:length(jur_list)){
   plot<-ggplot(subset(unittype_jur, unittype_jur$jurisdiction_id==jur_list[i]),  
-               aes(x=yr, y=N, group=as.factor(unittype), color=as.factor(unittype))) +
+               aes(x=yr, y=N_chg, group=as.factor(unittype), color=as.factor(unittype))) +
     geom_bar(stat = "identity") +
     labs(title=paste("Households by Jurisdiction by Unit Type\n", jur_list2[i]), y="Households by Unit Type category", x="Year",
         caption="Source: isam.xpef03.household+data_cafe.regional_forecast.sr13_final.mgra13")+
