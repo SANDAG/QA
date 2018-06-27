6066df12fc83e1deadc5446ab7e501a7cbdeca88
SANDAGNET\dco <Debbie.correia@sandag.org>
2018-06-27 11:18:45 -0700

    Fix Format

--- SR14Forecast/SubRegional/Total Household Population.R
+++ SR14Forecast/SubRegional/Total Household Population.R
@@ -75,6 +75,9 @@ for(i in jur_list) { #1:length(unique(hh_jur[["cityname"]]))){
           legend.title=element_blank())
   # ggsave(plot, file= paste(results, 'Total Household Pop',  i, ".png", sep=''))#, scale=2)
   output_table<-data.frame(plotdat$yr,plotdat$N,plotdat$N_chg,plotdat$reg,plotdat$regN)
+  output_table$plotdat.N_chg[output_table$plotdat.yr == 'y2016'] <- ''
+  output_table$plotdat.reg[output_table$plotdat.yr == 'y2016'] <- ''
+  hhtitle = paste("Households Pop ",i,sep='')
   setnames(output_table, old=c("plotdat.yr","plotdat.N","plotdat.N_chg","plotdat.reg","plotdat.regN"),new=c("Year","Total","Abs. Chg.","Reg abs. chg.","Reg Total"))
   tt <- ttheme_default(colhead=list(fg_params = list(parse=TRUE)))
   tbl <- tableGrob(output_table, rows=NULL, theme=tt)
@@ -130,6 +133,9 @@ for(i in cpa_list3) { #1:length(unique(hh_cpa[["cpaname"]]))){
           legend.title=element_blank())
   # ggsave(plot, file= paste(results, 'Total Household Pop',  i, ".png", sep=''))#, scale=2)
   output_table<-data.frame(plotdat$yr,plotdat$N,plotdat$N_chg,plotdat$reg,plotdat$regN)
+  output_table$plotdat.N_chg[output_table$plotdat.yr == 'y2016'] <- ''
+  output_table$plotdat.reg[output_table$plotdat.yr == 'y2016'] <- ''
+  hhtitle = paste("Households Pop ",i,sep='')
   setnames(output_table, 
            old=c("plotdat.yr","plotdat.N","plotdat.N_chg","plotdat.reg","plotdat.regN"),new=c("Year","Total","Abs. Chg.",
               "Reg abs. chg.","Reg Total"))
