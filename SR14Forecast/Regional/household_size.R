aea9d91e867ad61a0798478d5da09895fecd423d
Anne Kuller <aku@sandag.org>
2018-06-28 12:11:51 -0700

    add output table for hhsize

--- SR14Forecast/Regional/household_size.R
+++ SR14Forecast/Regional/household_size.R
@@ -21,6 +21,9 @@ odbcClose(channel)
 head(hh)
 hh<- hh[order(hh$geotype,hh$geozone,hh$yr_id),]
 
+hh$year<- "y"
+hh$yr <- as.factor(paste(hh$year, hh$yr, sep = ""))
+
 hh_jur = subset(hh,geotype=='jurisdiction')
 colnames(hh_jur)[colnames(hh_jur)=="geozone"] <- "cityname"
 
@@ -30,24 +33,26 @@ colnames(hh_cpa)[colnames(hh_cpa)=="geozone"] <- "cpaname"
 hh_region = subset(hh,geotype=='region')
 colnames(hh_region)[colnames(hh_region)=="geozone"] <- "SanDiegoRegion"
 
-hh_jur$reg<-hh_region[match(hh_jur$yr_id, hh_region$yr_id),6]
-hh_cpa$reg<-hh_region[match(hh_cpa$yr_id, hh_region$yr_id),6]
+hh_jur$reg_hhs<-hh_region[match(hh_jur$yr_id, hh_region$yr_id),6]
+hh_jur$reg_hh<-hh_region[match(hh_jur$yr_id, hh_region$yr_id),4]
+hh_jur$reg_hhp<-hh_region[match(hh_jur$yr_id, hh_region$yr_id),5]
+
+hh_cpa$reg_hhs<-hh_region[match(hh_cpa$yr_id, hh_region$yr_id),6]
+hh_cpa$reg_hh<-hh_region[match(hh_cpa$yr_id, hh_region$yr_id),4]
+hh_cpa$reg_hhp<-hh_region[match(hh_cpa$yr_id, hh_region$yr_id),5]
 
 
 maindir = dirname(rstudioapi::getSourceEditorContext()$path)
 results<-"plots\\hhsize\\jur\\"
 ifelse(!dir.exists(file.path(maindir,results)), dir.create(file.path(maindir,results), showWarnings = TRUE, recursive=TRUE),0)
 
-hh_jur$year<- "y"
-hh_jur$yr <- as.factor(paste(hh_jur$year, hh_jur$yr, sep = ""))
-
 jur_list = unique(hh_jur[["cityname"]])
 
 for(i in jur_list) {
   plotdat = subset(hh_jur, hh_jur$cityname==i)
   plot<-ggplot(plotdat, aes(yr)) + 
-    geom_line(aes(y = reg, colour = "region",group=0),size=1.5) +
-    geom_point(size=3,aes(y=reg,color="region")) +
+    geom_line(aes(y = reg_hhs, colour = "region",group=0),size=1.5) +
+    geom_point(size=3,aes(y=reg_hhs,color="region")) +
     geom_line(aes(y = hhs, colour = cityname,group=0),size=1.5) + 
     geom_point(size=3,aes(y=hhs,colour=cityname)) +
     scale_y_continuous(limits = c(2, 3.5)) +
@@ -60,15 +65,20 @@ for(i in jur_list) {
          y="Household size", x="Year",
          caption="Sources: demographic_warehouse.fact.population,demographic_warehouse.dim.mgra, housing.datasource_id=14")
     results<-"plots\\hhsize\\jur\\"
-    ggsave(plot, file= paste(results, 'hhsize', i, ".png", sep=''),
+    output_table<-plotdat[,c("yr_id","hhp","households","hhs","reg_hhp","reg_hh","reg_hhs")]
+    colnames(output_table)[colnames(output_table)=="households"] <- "hh"
+    tt <- ttheme_default(base_size=8,colhead=list(fg_params = list(parse=TRUE)))
+    tbl <- tableGrob(output_table, rows=NULL, theme=tt)
+    lay <- rbind(c(1,1,1,1,1),
+                 c(1,1,1,1,1),
+                 c(1,1,1,1,1),
+                 c(2,2,2,2,2),
+                 c(2,2,2,2,2))
+    output<-grid.arrange(plot,tbl,ncol=1,as.table=TRUE,layout_matrix=lay)
+    ggsave(output, file= paste(results, 'hhsize', i, ".png", sep=''),
          width=6, height=8, dpi=100)#, scale=2)
 }
 
-
-
-hh_cpa$year<- "y"
-hh_cpa$yr <- as.factor(paste(hh_cpa$year, hh_cpa$yr, sep = ""))
-hh_cpa$N <-  hh_cpa$households
 
 cpa_list = unique(hh_cpa[["cpaname"]])
 
@@ -79,8 +89,8 @@ ifelse(!dir.exists(file.path(maindir,results)), dir.create(file.path(maindir,res
 for(i in cpa_list) { 
   plotdat = subset(hh_cpa, hh_cpa$cpaname==i)
   plot<-ggplot(plotdat, aes(yr)) + 
-    geom_line(aes(y = reg, colour = "region",group=0),size=1.5) +
-    geom_point(size=3,aes(y=reg,color="region")) +
+    geom_line(aes(y = reg_hhs, colour = "region",group=0),size=1.5) +
+    geom_point(size=3,aes(y=reg_hhs,color="region")) +
     theme(legend.position = "bottom",
           legend.title=element_blank()) +
     geom_line(aes(y = hhs, colour = cpaname,group=0),size=1.5) + 
@@ -98,7 +108,17 @@ for(i in cpa_list) {
   i = gsub("\\-","_",i)
   i = gsub("\\:","_",i)
   results<-"plots\\hhsize\\cpa\\"
-  ggsave(plot, file= paste(results, 'hhsize', i, ".png", sep=''),
+  output_table<-plotdat[,c("yr_id","hhp","households","hhs","reg_hhp","reg_hh","reg_hhs")]
+  colnames(output_table)[colnames(output_table)=="households"] <- "hh"
+  tt <- ttheme_default(base_size=8,colhead=list(fg_params = list(parse=TRUE)))
+  tbl <- tableGrob(output_table, rows=NULL, theme=tt)
+  lay <- rbind(c(1,1,1,1,1),
+               c(1,1,1,1,1),
+               c(1,1,1,1,1),
+               c(2,2,2,2,2),
+               c(2,2,2,2,2))
+  output<-grid.arrange(plot,tbl,ncol=1,as.table=TRUE,layout_matrix=lay)
+  ggsave(output, file= paste(results, 'hhsize', i, ".png", sep=''),
          width=6, height=8, dpi=100)#, scale=2)
 }
 
