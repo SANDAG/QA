84572d8e084e3c947d2119e99956c2e911ee8d08
lisbethhow <lisbeth.howard@sandag.org>
2018-07-09 12:13:59 -0700

    save out additional files by geozone and revise column numbers

--- SR14Forecast/Regional/age_ethn_gender.R
+++ SR14Forecast/Regional/age_ethn_gender.R
@@ -21,6 +21,8 @@ odbcClose(channel)
 
 tail(dem)
 
+write.csv(dem, paste("M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\4_Data Files\\dem_sql",format(Sys.time(), "_%Y%m%d_%H%M%S"),".csv",sep=""))
+
 #change all factors to character for ease of coding
 options(stringsAsFactors=FALSE)
 
@@ -108,8 +110,9 @@ dem_gender$N_pct<-round(dem_gender$N_pct,digits=2)
 dem_gender$geozone_pop<-geozone_pop[match(paste(dem_gender$yr_id, dem_gender$geozone), paste(geozone_pop$yr_id, geozone_pop$geozone)), 4]
 dem_gender$pct_of_total<-(dem_gender$pop / dem_gender$geozone_pop)*100
 dem_gender$pct_of_total<-round(dem_gender$pct_of_total,digits=2)
-setnames(dem_age, old=c("sex", "yr_id", "pop"),new=c("Gender", "Year", "Population"))
-dem_gender <- rename(dem_gender, c("F"="Female", "M"="Male"))
+setnames(dem_gender, old=c("sex", "yr_id", "pop"),new=c("Gender", "Year", "Population"))
+dem_gender$Gender[dem_gender$Gender=="F"]<- "Female"
+dem_gender$Gender[dem_gender$Gender=="M"]<- "Male"
 
 
 head(dem_ethn)
@@ -120,7 +123,7 @@ dem_ethn$N_pct<-round(dem_ethn$N_pct,digits=2)
 dem_ethn$geozone_pop<-geozone_pop[match(paste(dem_ethn$yr_id, dem_ethn$geozone), paste(geozone_pop$yr_id, geozone_pop$geozone)), 4]
 dem_ethn$pct_of_total<-(dem_ethn$pop / dem_ethn$geozone_pop)*100
 dem_ethn$pct_of_total<-round(dem_ethn$pct_of_total,digits=2)
-setnames(dem_age, old=c("short_name", "yr_id", "pop"),new=c("Ethnicity", "Year", "Population"))
+setnames(dem_ethn, old=c("short_name", "yr_id", "pop"),new=c("Ethnicity", "Year", "Population"))
 
 
 #recode NA values for 2016 change
@@ -142,10 +145,22 @@ dem_ethn$N_pct[dem_ethn$N_chg == "NA"] <- 0
 dem_ethn$N_pct[dem_ethn$N_pct == "NA"] <- 0
 dem_ethn$pct_of_total[dem_ethn$pct_of_total == "NaN"] <- 0
 
+
+
 #create files for the region
 dem_age_region = subset(dem_age,geotype=='region')
 dem_gender_region = subset(dem_gender,geotype=='region')
 dem_ethn_region = subset(dem_ethn,geotype=='region')
+
+#create files for jurisdictions
+dem_age_jurisdiction = subset(dem_age,geotype=='jurisdiction')
+dem_gender_jurisdiction = subset(dem_gender,geotype=='jurisdiction')
+dem_ethn_jurisdiction = subset(dem_ethn,geotype=='jurisdiction')
+
+#create files for cpas
+dem_age_cpa = subset(dem_age,geotype=='cpa')
+dem_gender_cpa = subset(dem_gender,geotype=='cpa')
+dem_ethn_cpa = subset(dem_ethn,geotype=='cpa')
 
 
 
@@ -157,9 +172,17 @@ write.csv(dem_age_region, "M:\\Technical Services\\QA Documents\\Projects\\Sub R
 write.csv(dem_gender_region, "M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\4_Data Files\\dem_gender_region.csv" )
 write.csv(dem_ethn_region, "M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\4_Data Files\\dem_ethn_region.csv" )
 
+write.csv(dem_age_jurisdiction, "M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\4_Data Files\\dem_age_jurisdiction.csv" )
+write.csv(dem_gender_jurisdiction, "M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\4_Data Files\\dem_gender_jurisdiction.csv" )
+write.csv(dem_ethn_jurisdiction, "M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\4_Data Files\\dem_ethn_jurisdiction.csv" )
 
-results<-"data\\demographics\\"
-ifelse(!dir.exists(file.path(maindir,results)), dir.create(file.path(maindir,results), showWarnings = TRUE, recursive=TRUE),0)
+write.csv(dem_age_cpa, "M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\4_Data Files\\dem_age_cpa.csv" )
+write.csv(dem_gender_cpa, "M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\4_Data Files\\dem_gender_cpa.csv" )
+write.csv(dem_ethn_cpa, "M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\4_Data Files\\dem_ethn_cpa.csv" )
 
-write.csv(dem_age, file= paste(results, 'dem_age.csv"))
+#the save to the sourcetree location needs to be fixed
+#results<-"data\\demographics\\"
+#ifelse(!dir.exists(file.path(maindir,results)), dir.create(file.path(maindir,results), showWarnings = TRUE, recursive=TRUE),0)
+
+#write.csv(dem_age, file= paste(results, 'dem_age.csv"))
 
