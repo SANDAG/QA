18728de0b218a34887a0fbcce4e98e681b4a190a
lisbethhow <lisbeth.howard@sandag.org>
2018-07-31 17:11:16 -0700

    save SQL script for total population data

---
+++ SR14Forecast/Queries/total_population.sql
@@ -0,0 +1,11 @@
+SELECT yr_id
+	,mgra.geotype
+	,mgra.geozone
+	,SUM(population) as pop
+FROM fact.population
+	INNER JOIN dim.mgra
+	ON mgra.mgra_id = population.mgra_id
+	AND mgra.geotype IN ('jurisdiction', 'cpa', 'region')
+WHERE datasource_id = 16
+GROUP BY yr_id, mgra.geotype, mgra.geozone
+ORDER BY yr_id, mgra.geotype, mgra.geozone
