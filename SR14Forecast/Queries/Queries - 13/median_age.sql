ee3ab87cd7808b2f742bb2972112de20aeb8778c
SANDAGNET\dco <Debbie.correia@sandag.org>
2018-08-20 10:13:43 -0700

    Series 13

---
+++ SR14Forecast/Queries/Queries - 13/median_age.sql
@@ -0,0 +1,5 @@
+Use demographic_warehouse
+
+EXECUTE dbo.compute_median_age_all_zones 13, 'jurisdiction'
+EXECUTE dbo.compute_median_age_all_zones 13, 'cpa'
+EXECUTE dbo.compute_median_age_all_zones 13, 'region'
