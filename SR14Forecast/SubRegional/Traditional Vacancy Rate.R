d49ae928cba17412a25894284627eaba7d89c6ec
SANDAGNET\dco <Debbie.correia@sandag.org>
2018-09-11 15:03:39 -0700

    Revise Vacancy script to include vacancy by unit type

--- SR14Forecast/SubRegional/Traditional Vacancy Rate.R
+++ SR14Forecast/SubRegional/Traditional Vacancy Rate.R
@@ -34,33 +34,33 @@ vacancy$geozone <- gsub("\\-","_",vacancy$geozone)
 vacancy$geozone <- gsub("\\:","_",vacancy$geozone)
 
 #This aggregates from type of units
-vac <-aggregate(cbind(units, hh, unoccupiable) ~yr_id + geozone + geotype, data= vacancy, sum,na.rm = TRUE)
+#vac <-aggregate(cbind(units, hh, unoccupiable) ~yr_id + geozone + geotype, data= vacancy, sum,na.rm = TRUE)
 
 
 #calculate the vacancy rate - formula does not exclude unoccupiable units
-vac$available <-(vac$units-vac$hh)
-vac$rate <-(vac$available/vac$units)*100
-vac$rate <-round(vac$rate,digits=2)
+vacancy$available <-(vacancy$units-vacancy$hh)
+vacancy$rate <-(vacancy$available/vacancy$units)*100
+vacancy$rate <-round(vacancy$rate,digits=2)
 
 
-head(vac)
-#vacancy$long_name<-' '
+head(vacancy)
+vacancy$long_name<-' '
 
-#vacancy$long_name[vacancy$short_name=="mf"]<- "Multi Family"
-#vacancy$long_name[vacancy$short_name=="mh"]<- "Mobile Home"
-#vacancy$long_name[vacancy$short_name=="sf"]<- "Single Family"
-#vacancy$long_name[vacancy$short_name=="sfmu"]<- "Single Family Multi Unit"
+vacancy$long_name[vacancy$short_name=="mf"]<- "Multi Family"
+vacancy$long_name[vacancy$short_name=="mh"]<- "Mobile Home"
+vacancy$long_name[vacancy$short_name=="sfd"]<- "Single Family"
+vacancy$long_name[vacancy$short_name=="sfa"]<- "Single Family Multi Unit"
 
-#vacancy$long_name<- factor(vacancy$long_name, levels = c("Multi Family",
-                                       #"Mobile Home","Single Family", "Single Family Multi Unit"))
+vacancy$long_name<- factor(vacancy$long_name, levels = c("Multi Family",
+                                       "Mobile Home","Single Family", "Single Family Multi Unit"))
 
 
-vac$year<- "y"
-vac$yr <- as.factor(paste(vac$year, vac$yr, sep = ""))
+vacancy$yr<- "y"
+vacancy$year <- as.factor(paste(vacancy$year, vacancy$yr, sep = ""))
 
-vac_jur = subset(vac,geotype=='jurisdiction')
-vac_cpa = subset(vac,geotype=='cpa')
-vac_region = subset(vac,geotype=='region')
+vacancy_jur = subset(vacancy,geotype=='jurisdiction')
+vacancy_cpa = subset(vacancy,geotype=='cpa')
+vacancy_region = subset(vacancy,geotype=='region')
 
 
 maindir = dirname(rstudioapi::getSourceEditorContext()$path)
@@ -68,10 +68,9 @@ results<-"plots\\Vacancy\\Jur\\"
 ifelse(!dir.exists(file.path(maindir,results)), dir.create(file.path(maindir,results), showWarnings = TRUE, recursive=TRUE),0)
 
 
-tail(vac_region)
+tail(vacancy_region)
 
 ##Jurisdiction plots and tables
-
 
 
 jur_list<- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19)
@@ -79,13 +78,15 @@ jur_list2<- c("Carlsbad","Chula Vista","Coronado","Del Mar","El Cajon","Encinita
               "National City","Oceanside","Poway","San Diego","San Marcos","Santee","Solana Beach","Vista","Unincorporated")
 
 citynames <- data.frame(jur_list, jur_list2)
-vac_jur$jurisdiction_id<-citynames[match(vac_jur$geozone, citynames$jur_list2),1]
-vac_jur$reg<-vac_region[match(vac_jur$yr_id, vac_region$yr_id),8]
+vacancy_jur$jurisdiction_id<-citynames[match(vacancy_jur$geozone, citynames$jur_list2),1]
+vacancy_jur$reg<-vacancy_region[match(vacancy_jur$yr_id, vacancy_region$yr_id),11]
+
+Vacancy_Jur_reshape <- reshape(select(vacancy_jur, yr_id, geotype, geozone, long_name, rate, reg), idvar = c("yr_id", "geozone", "geotype", "reg"), timevar = "long_name", direction = "wide")
 
 
 for(i in jur_list) { 
-plotdat = subset(vac_jur, vac_jur$jurisdiction_id==jur_list[i])
-plot<- ggplot(plotdat, aes(x=yr_id, y=rate, colour=geozone))+
+plotdat = subset(vacancy_jur, vacancy_jur$jurisdiction_id==jur_list[i])
+plot<- ggplot(plotdat, aes(x=yr_id, y=rate, colour="Jurisdiction"))+
  geom_line(size=1)+
   geom_line(aes(x=yr_id, y=reg, colour="Region")) +
   scale_y_continuous(labels = comma, limits=c(0,10))+
@@ -98,19 +99,19 @@ plot<- ggplot(plotdat, aes(x=yr_id, y=rate, colour=geozone))+
         legend.title=element_blank(),
         plot.caption = element_text(size = 7))
 ggsave(plot, file= paste(results, 'vacancy', jur_list2[i], "16.png", sep=''))#, scale=2)
-#sortdat <- plotdat[order(plotdat$geozone,plotdat$yr_id),]
-output_table<-data.frame(plotdat$yr_id,plotdat$rate,plotdat$reg)
-setnames(output_table, old=c("plotdat.yr_id","plotdat.rate","plotdat.reg"),new=c("Year","Jur Vac Rate","Reg Vac Rate"))
+sortdat <- plotdat[order(plotdat$geozone,plotdat$yr_id),]
+output_table<-data.frame(plotdat$yr_id, plotdat$long_name, plotdat$rate,plotdat$reg)
+setnames(output_table, old=c("plotdat.yr_id","plotdat.long_name", "plotdat.rate","plotdat.reg"),new=c("Year","Unit Type", "Jur Vac Rate","Reg Vac Rate"))
 tt <- ttheme_default(base_size=9,colhead=list(fg_params = list(parse=TRUE)))
 #tt <- ttheme_default(core = list(fg_params=list(cex = 1.0)),
- #                    colhead = list(fg_params=list(cex = 1.0)),
-  #                   rowhead = list(fg_params=list(cex = 1.0)))
+                   #colhead = list(fg_params=list(cex = 1.0)),
+                    #rowhead = list(fg_params=list(cex = 1.0)))
 tbl <- tableGrob(output_table, rows=NULL, theme=tt)
-lay <- rbind(c(1,1,1,1,1),
-             c(1,1,1,1,1),
-             c(2,2,2,2,2),
-             c(2,2,2,2,2))
-output<-grid.arrange(plot,tbl,as.table=TRUE,layout_matrix=lay)
+#lay <- rbind(c(1,1,1,1,1),
+             #c(1,1,1,1,1),
+             #c(2,2,2,2,2),
+             #c(2,2,2,2,2))
+output<-grid.arrange(plot,tbl,as.table=TRUE)#layout_matrix=lay)
 ggsave(output, file= paste(results,'vacancy',jur_list2[i], "16.png", sep=''))#, scale=2))
 }
 
@@ -123,17 +124,17 @@ head(plotdat)
 results<-"plots\\Vacancy\\CPA\\"
 ifelse(!dir.exists(file.path(maindir,results)), dir.create(file.path(maindir,results), showWarnings = TRUE, recursive=TRUE),0)
 
-cpa_list = unique(vac_cpa[["geozone"]])
+cpa_list = unique(vacancy_cpa[["geozone"]])
 
-#vac_jur$jurisdiction_id<-citynames[match(vac_jur$geozone, citynames$jur_list2),1]
-vac_cpa$reg<-vac_region[match(vac_cpa$yr_id, vac_region$yr_id),8]
+#vacancy_jur$jurisdiction_id<-citynames[match(vacancy_jur$geozone, citynames$jur_list2),1]
+vacancy_cpa$reg<-vacancy_region[match(vacancy_cpa$yr_id, vacancy_region$yr_id),8]
 
 
-head(vac_cpa)
+head(vacancy_cpa)
 
 for(i in 1:length(cpa_list)) { 
-  plotdat = subset(vac_cpa, vac_cpa$geozone==cpa_list[i])
-  plot<- ggplot(plotdat, aes(x=yr_id, y=rate, colour=geozone))+
+  plotdat = subset(vacancy_cpa, vacancy_cpa$geozone==cpa_list[i])
+  plot<- ggplot(plotdat, aes(x=yr_id, y=rate, colour="CPA"))+
     geom_line(size=1)+
     geom_line(aes(x=yr_id, y=reg, colour="Region")) +
     scale_y_continuous(labels = comma, limits=c(0,10))+
