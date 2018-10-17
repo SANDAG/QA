a675b0b1bcd2de8fd961cb9c62c97e3b15b556a3
SANDAGNET\dco <Debbie.correia@sandag.org>
2018-10-17 09:38:13 -0700

    QS-1 comparing source 18 to 17 fixe

--- SR14Forecast/Regional/hh_variables_18to17.R
+++ SR14Forecast/Regional/hh_variables_18to17.R
@@ -11,14 +11,15 @@ pkgTest(packages)
 
 #setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-hh_var18<-read.csv("M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\Results\\Phase 5\\hh_hhp_hhs_hu_vac_age_comparison.csv")
-hh_var17<-read.csv("M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\Results\\Phase 5\\hh_hhp_hhs_hu_vac_age_comparison17.csv")
+hh_var18<-read.csv("M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\Results\\Phase 5\\hh_hhp_hhs_hu_vac_age_comparison.csv", stringsAsFactors = FALSE)
+hh_var17<-read.csv("M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\Results\\Phase 5\\hh_hhp_hhs_hu_vac_age_comparison17.csv", stringsAsFactors = FALSE)
 
 hh_var17[c("hh_numchg", "hhp_numchg", "hhs_numchg", "hh_pctchg", "hhp_pctchg", "hhs_pctchg", "units_numchg", "units_pctchg", "vac_numchg", "vac_pctchg", "age_numchg", "age_pctchg")]<-list(NULL)
 hh_var18[c("hh_numchg", "hhp_numchg", "hhs_numchg", "hh_pctchg", "hhp_pctchg", "hhs_pctchg", "units_numchg", "units_pctchg", "vac_numchg", "vac_pctchg", "age_numchg", "age_pctchg")]<-list(NULL)
 
-
-hh_var17$geozone Rancho PeÃ±asquitos
+hh_var17$geozone[hh_var17$geotype=="region"]<- "Region"
+hh_var17$geozone[hh_var17$geozone=="Los PeÃ±asquitos Canyon Preserve"]<- "Los Peñasquitos Canyon Preserve"
+hh_var17$geozone[hh_var17$geozone=="Rancho PeÃ±asquitos"]<- "Rancho Peñasquitos"
 
 hh_var17$geozone <- gsub("\\*","",hh_var17$geozone)
 hh_var17$geozone <- gsub("\\-","_",hh_var17$geozone)
@@ -47,26 +48,51 @@ class(hh_var17$yr_id)
 class(hh_var18$geozone)
 class(hh_var17$geozone)
 
+hh_var_17_18$households_numchg<- hh_var_17_18$households_18 - hh_var_17_18$households_17
+hh_var_17_18$hhp_numchg<- hh_var_17_18$hhp_18 - hh_var_17_18$hhp_17
+hh_var_17_18$hhs_numchg<- hh_var_17_18$hhs_18 - hh_var_17_18$hhs_17
+hh_var_17_18$units_numchg<- hh_var_17_18$units_18 - hh_var_17_18$units_17
+hh_var_17_18$unoccupiable_numchg<- hh_var_17_18$unoccupiable_18 - hh_var_17_18$unoccupiable_17
+hh_var_17_18$vac_rate_numchg<- hh_var_17_18$vac_rate_18 - hh_var_17_18$vac_rate_17
+hh_var_17_18$median_age_numchg<- hh_var_17_18$median_age_18 - hh_var_17_18$median_age_17
 
-households_17 households_18
+hh_var_17_18$hh_pctchg<- ((hh_var_17_18$households_17 - hh_var_17_18$households_18)/hh_var_17_18$households_17)*100
+hh_var_17_18$hhp_pctchg<- ((hh_var_17_18$hhp_17 - hh_var_17_18$hhp_18)/hh_var_17_18$hhp_17)*100
+hh_var_17_18$hhs_pctchg<- ((hh_var_17_18$hhs_17 - hh_var_17_18$hhs_18)/hh_var_17_18$hhs_17)*100
+hh_var_17_18$units_pctchg<- ((hh_var_17_18$units_17 - hh_var_17_18$units_18)/hh_var_17_18$units_17)*100
+hh_var_17_18$unoccupiable_pctchg<- ((hh_var_17_18$unoccupiable_17 - hh_var_17_18$unoccupiable_18)/hh_var_17_18$unoccupiable_17) *100
+hh_var_17_18$vac_rate_pctchg<- ((hh_var_17_18$vac_rate_17 - hh_var_17_18$vac_rate_18)/hh_var_17_18$vac_rate_17) *100
+hh_var_17_18$median_age_pctchg<- ((hh_var_17_18$median_age_17 - hh_var_17_18$median_age_18)/hh_var_17_18$median_age_17)*100
 
-hhp_17 hhp_18
-
-hhs_17 hhs_18
-
-units_17 units_18
-
-unoccupiable_17 unoccupiable_18
-
-vac_rate_17 vac_rate_18 
-
-median_age_17 median_age_18
-
-
-
-
+hh_var_17_18$hh_pctchg<-round(hh_var_17_18$hh_pctchg,digits=2)
+hh_var_17_18$hhp_pctchg<-round(hh_var_17_18$hhp_pctchg,digits=2)
+hh_var_17_18$hhs_pctchg<-round(hh_var_17_18$hhs_pctchg,digits=2)
+hh_var_17_18$units_pctchg<-round(hh_var_17_18$units_pctchg,digits=2)
+hh_var_17_18$unoccupiable_pctchg<-round(hh_var_17_18$unoccupiable_pctchg,digits=2)
+hh_var_17_18$vac_rate_pctchg<-round(hh_var_17_18$vac_rate_pctchg,digits=2)
+hh_var_17_18$median_age_pctchg<-round(hh_var_17_18$median_age_pctchg,digits=2)
 
 
+Households<- hh_var_17_18[c("yr_id","geotype", "geozone", "households_17", "households_18", "households_numchg","hh_pctchg")]
+Household_pop <- hh_var_17_18[c("yr_id","geotype", "geozone", "hhp_17", "hhp_18", "hhp_numchg", "hhp_pctchg")]
+Household_Size<- hh_var_17_18[c("yr_id","geotype", "geozone", "hhs_17", "hhs_18", "hhs_numchg", "hhs_pctchg")]
+Units<- hh_var_17_18[c("yr_id","geotype", "geozone", "units_17", "units_18", "units_numchg", "units_pctchg")]
+unoccupiable<- hh_var_17_18[c("yr_id","geotype", "geozone", "unoccupiable_17","unoccupiable_18", "unoccupiable_numchg", "unoccupiable_pctchg")]
+Vacancy_rate<- hh_var_17_18[c("yr_id","geotype", "geozone", "vac_rate_17", "vac_rate_18", "vac_rate_numchg", "vac_rate_pctchg")]
+Median_Age<- hh_var_17_18[c("yr_id","geotype", "geozone", "median_age_17", "median_age_18", "median_age_numchg", "median_age_pctchg")]
 
+
+write.csv(Households, "Households.csv")
+write.csv(Household_pop, "Household_pop.csv")
+write.csv(Household_Size, "Household_Size.csv")
+write.csv(Units, "Units.csv")
+write.csv(unoccupiable, "unoccupiable.csv")
+write.csv(Vacancy_rate, "Vacancy_rate.csv")
+write.csv(Median_Age, "Median_Age.csv")
+
+summary(hh_var_17_18$units_numchg)
+
+
+Units_FlagAger
 
 
