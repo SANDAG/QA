3783cd94b2fcbc7b858e3df975a05e6ef34e9b63
lisbethhow <lisbeth.howard@sandag.org>
2018-11-02 17:09:56 -0700

    prepare R script for median income comparison

--- SR14Forecast/Regional/Income Comparison Prep.R
+++ SR14Forecast/Regional/Income Comparison Prep.R
@@ -13,55 +13,102 @@ pkgTest(packages)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 source("../Queries/readSQL.R")
 
-Income13_2020<- read.csv("T:\\ABM\\release\\ABM\\archive\\version_13.2.2\\input\\2020\\mgra13_based_input2020.csv", stringsAsFactors = FALSE)
-Income13_2025<- read.csv("T:\\ABM\\release\\ABM\\archive\\version_13.2.2\\input\\2025\\mgra13_based_input2025.csv", stringsAsFactors = FALSE)
-Income13_2035<- read.csv("T:\\ABM\\release\\ABM\\archive\\version_13.2.2\\input\\2035\\mgra13_based_input2035.csv", stringsAsFactors = FALSE)
-Income13_2050<- read.csv("T:\\ABM\\release\\ABM\\archive\\version_13.2.2\\input\\2050\\mgra13_based_input2050.csv", stringsAsFactors = FALSE)
+#bring data in from SQL
+#channel <- odbcDriverConnect('driver={SQL Server};server=sql2014a8; database=data_cafe; trusted_connection=true')
+#geo_sql = getSQL("../Queries/geography.sql")
+#geography<-sqlQuery(channel,geo_sql)
+#odbcClose(channel)
+
+inc_abm_13_2020<- read.csv("T:\\ABM\\release\\ABM\\archive\\version_13.2.2\\input\\2020\\mgra13_based_input2020.csv", stringsAsFactors = FALSE)
+inc_abm_13_2025<- read.csv("T:\\ABM\\release\\ABM\\archive\\version_13.2.2\\input\\2025\\mgra13_based_input2025.csv", stringsAsFactors = FALSE)
+inc_abm_13_2035<- read.csv("T:\\ABM\\release\\ABM\\archive\\version_13.2.2\\input\\2035\\mgra13_based_input2035.csv", stringsAsFactors = FALSE)
+inc_abm_13_2050<- read.csv("T:\\ABM\\release\\ABM\\archive\\version_13.2.2\\input\\2050\\mgra13_based_input2050.csv", stringsAsFactors = FALSE)
 
 
-Income13_2020$yr = 2020
-Income13_2025$yr = 2025
-Income13_2035$yr = 2035
-Income13_2050$yr = 2050
+inc_abm_13_2020$yr = 2020
+inc_abm_13_2025$yr = 2025
+inc_abm_13_2035$yr = 2035
+inc_abm_13_2050$yr = 2050
 
-Income13<-rbind(Income13_2020,Income13_2025,Income13_2035,Income13_2050)
+inc_abm_13<-rbind(inc_abm_13_2020,inc_abm_13_2025,inc_abm_13_2035,inc_abm_13_2050)
 
-Income13$cat1<- Income13$i1 + Income13$i2
-Income13$cat2<- Income13$i3 + Income13$i4
-Income13$cat3<- Income13$i5 + Income13$i6
-Income13$cat4<- Income13$i7 + Income13$i8
-Income13$cat5<- Income13$i9 + Income13$i10
+inc_abm_13$cat1<- inc_abm_13$i1 + inc_abm_13$i2
+inc_abm_13$cat2<- inc_abm_13$i3 + inc_abm_13$i4
+inc_abm_13$cat3<- inc_abm_13$i5 + inc_abm_13$i6
+inc_abm_13$cat4<- inc_abm_13$i7 + inc_abm_13$i8
+inc_abm_13$cat5<- inc_abm_13$i9 + inc_abm_13$i10
+
+inc_abm_13<-select(inc_abm_13, mgra, hh, yr, cat1, cat2, cat3, cat4, cat5)
+
+#SQL query isn't working above so currently accesses file from project folder
+geography <-read.csv("M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\4_Data Files\\Phase 4\\median_income\\geography.csv", stringsAsFactors = FALSE)
+colnames(geography)[1]<-"mgra_13"
+
+geography$cocpa_13<-as.numeric(geography$cocpa_13)
+geography$cicpa_13<-as.numeric(geography$cicpa_13)
+
+geography$cpa_13<- ifelse(is.na(geography$cicpa_13) & !is.na(geography$cocpa_13), geography$cocpa_13, geography$cicpa_13)
+
+summary(geography$cpa_13)
+head(geography)
+
+inc_abm_13 <- merge(inc_abm_13, geography, by.x="mgra", by.y="mgra_13")
+#areas not in a CPA are coded as 9
+inc_abm_13$cpa_[is.na(inc_abm_13$cpa_13)]<- 9
+
+inc_abm_13_cpa <- aggregate (cbind(cat1, cat2, cat3, cat4, cat5)~cpa_13+yr, data=inc_abm_13, sum)
+inc_abm_13_cpa_melt <- melt(inc_abm_13_cpa, id.vars=c("cpa_13", "yr"))
+setnames(inc_abm_13_cpa_melt,old=c("variable","value"),new=c("income_group_id","hh"))
+
+inc_abm_13_cpa_melt$income_group_id <-as.character(inc_abm_13_cpa_melt$income_group_id)
+
+inc_abm_13_cpa_melt$income_group_id[inc_abm_13_cpa_melt$income_group_id=="cat1"]<- "1"
+inc_abm_13_cpa_melt$income_group_id[inc_abm_13_cpa_melt$income_group_id=="cat2"]<- "2"
+inc_abm_13_cpa_melt$income_group_id[inc_abm_13_cpa_melt$income_group_id=="cat3"]<- "3"
+inc_abm_13_cpa_melt$income_group_id[inc_abm_13_cpa_melt$income_group_id=="cat4"]<- "4"
+inc_abm_13_cpa_melt$income_group_id[inc_abm_13_cpa_melt$income_group_id=="cat5"]<- "5"
 
 
-##Still need to save on M Drive
-Geo_Mgra13 <-read.csv("C:\\Users\\dco\\Desktop\\QA Version Control\\SR14Forecast\\Regional\\Geography_MGRA_13.csv", stringsAsFactors = FALSE)
+inc_abm_13_cpa_melt$lower_bound[inc_abm_13_cpa_melt$income_group_id=="1"]<- 0
+inc_abm_13_cpa_melt$upper_bound[inc_abm_13_cpa_melt$income_group_id=="1"]<- 29999
+inc_abm_13_cpa_melt$lower_bound[inc_abm_13_cpa_melt$income_group_id=="2"]<- 30000
+inc_abm_13_cpa_melt$upper_bound[inc_abm_13_cpa_melt$income_group_id=="2"]<- 59999
+inc_abm_13_cpa_melt$lower_bound[inc_abm_13_cpa_melt$income_group_id=="3"]<- 60000
+inc_abm_13_cpa_melt$upper_bound[inc_abm_13_cpa_melt$income_group_id=="3"]<- 99999
+inc_abm_13_cpa_melt$lower_bound[inc_abm_13_cpa_melt$income_group_id=="4"]<- 100000
+inc_abm_13_cpa_melt$upper_bound[inc_abm_13_cpa_melt$income_group_id=="4"]<- 149999
+inc_abm_13_cpa_melt$lower_bound[inc_abm_13_cpa_melt$income_group_id=="5"]<- 150000
+inc_abm_13_cpa_melt$upper_bound[inc_abm_13_cpa_melt$income_group_id=="5"]<- 349999
 
-Geo_Mgra13$cocpa_13<-as.numeric(Geo_Mgra13$cocpa_13)
-Geo_Mgra13$cicpa_13<-as.numeric(Geo_Mgra13$cicpa_13)
 
-Geo_Mgra13$cpa_13<- ifelse(is.na(Geo_Mgra13$cicpa_13) & !is.na(Geo_Mgra13$cocpa_13), Geo_Mgra13$cocpa_13, Geo_Mgra13$cicpa_13)
+#I added one to interval calculation because it looks like there is a rounding thing happening in SQL script results - keep or delete?
+inc_abm_13_cpa_melt$interval_width<-inc_abm_13_cpa_melt$upper_bound-inc_abm_13_cpa_melt$lower_bound +1
 
-summary(Geo_Mgra13$cpa_13)
 
-inc13geo <- merge(Income13, Geo_Mgra13, by.x="mgra", by.y="ï..mgra_13")
-inc13geo$cpa_13[is.na(inc13geo$cpa_13)]<- 9
-summary(inc13geo$cpa_13)
+inc_abm_13_cpa_melt<- inc_abm_13_cpa_melt[order(inc_abm_13_cpa_melt$cpa_13,inc_abm_13_cpa_melt$yr),]
 
-inc13_cpa <- aggregate (cbind(cat1, cat2, cat3, cat4, cat5)~cpa_13+yr, data= inc13geo, sum)
-inc13_cpa_melt <- melt(inc13_cpa, id.vars=c("cpa_13", "yr"))
-setnames(inc13_cpa_melt, old="variable", new="income_group_id")
+inc_abm_13_cpa_melt <- data.table(inc_abm_13_cpa_melt)
+inc_abm_13_cpa_melt[, cum_sum := cumsum(hh), by=list(yr, cpa_13)]
 
-as.character(inc13_cpa_melt$income_group_id)
+inc_abm_13_cpa_melt<-as.data.frame.matrix(inc_abm_13_cpa_melt) 
 
-inc13_cpa_melt$income_group_id[inc13_cpa_melt$income_group_id=="cat1"]<- "1"
+head(inc_abm_13_cpa_melt)
+
+
+##########
+#add formula for median income and exclude records of income groups below the group where the median will be found
+
+
+
+
+
+
+
 
 
 
 inc13_jur <- aggregate(cbind(cat1, cat2, cat3, cat4, cat5)~jurisdiction_2015+yr, data = inc13geo,sum)
 inc13_jur_melt <- melt(inc13_jur, id.vars=c("jurisdiction_2015", "yr"))
-
-
-
 
 inc13_reg <- aggregate (c(i1,i2,i3,i4,i5,i6,i7,i8,i9,i10)~+yr, data = inc13geo,sum)                       
 summary(Inc13geo$jurisdiction_2015)
@@ -72,44 +119,6 @@ class(inc13geo$i1)
 inc13_reg <- aggregate(i1~yr+jurisdiction_2015, data = inc13geo,sum) 
 
 
-channel <- odbcDriverConnect('driver={SQL Server}; server=sql2014a8; database=demographic_warehouse; trusted_connection=true')
-median_income_jur_sql = getSQL("../Queries/median_income_jur.sql")
-mi_jur<-sqlQuery(channel,median_income_jur_sql,stringsAsFactors = FALSE)
-median_income_cpa_sql = getSQL("../Queries/median_income_cpa.sql")
-mi_cpa<-sqlQuery(channel,median_income_cpa_sql,stringsAsFactors = FALSE)
-median_income_region_sql = getSQL("../Queries/median_income_region.sql")
-mi_region<-sqlQuery(channel,median_income_region_sql,stringsAsFactors = FALSE)
-odbcClose(channel)
-
-#save a time stamped verion of the raw file from SQL
-write.csv(mi_jur, paste("M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\4_Data Files\\time stamp files\\mijur_sql_17",format(Sys.time(), "_%Y%m%d_%H%M%S"),".csv",sep=""))
-write.csv(mi_cpa, paste("M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\4_Data Files\\time stamp files\\micpa_sql_17",format(Sys.time(), "_%Y%m%d_%H%M%S"),".csv",sep=""))
-write.csv(mi_region, paste("M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\4_Data Files\\time stamp files\\miregion_sql_17",format(Sys.time(), "_%Y%m%d_%H%M%S"),".csv",sep=""))
-
-head(mi_region)
-#remove unneeded characters from geozone values.
-mi_cpa$geozone <- gsub("\\*","",mi_cpa$geozone)
-mi_cpa$geozone <- gsub("\\-","_",mi_cpa$geozone)
-mi_cpa$geozone <- gsub("\\:","_",mi_cpa$geozone)
-
-mi_jur$reg<-mi_region[match(mi_jur$yr_id, mi_region$yr_id),"median_inc"]
-mi_cpa$reg<-mi_region[match(mi_cpa$yr_id, mi_region$yr_id),"median_inc"]
 
 
 
-
-
-
-
-
-
-Income14_2020<-read.csv("T:\\ABM\\release\\ABM\\version_14_0_1\\input\\2020\\households.csv", stringsAsFactors = FALSE)
-Income14_2025<-read.csv("T:\\ABM\\release\\ABM\\version_14_0_1\\input\\2025\\households.csv", stringsAsFactors = FALSE)
-Income14_2035<-read.csv("T:\\ABM\\release\\ABM\\version_14_0_1\\input\\2035\\households.csv", stringsAsFactors = FALSE)
-Income14_2050<-read.csv("T:\\ABM\\release\\ABM\\version_14_0_1\\input\\2050\\households.csv", stringsAsFactors = FALSE)
-
-
-Income14_2020$yr = 2020
-Income14_2025$yr = 2025
-Income14_2035$yr = 2035
-Income14_2050$yr = 2050
