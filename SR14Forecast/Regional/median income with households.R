878c27a34b7e08d960ebd7ca11b8ba83be99b80e
Anne Kuller <aku@sandag.org>
2018-11-25 15:13:53 -0800

    calculate median income for region sr13

--- SR14Forecast/Regional/median income with households.R
+++ SR14Forecast/Regional/median income with households.R
@@ -111,15 +111,30 @@ inc_abm_13_cpa$upper_bound[inc_abm_13_cpa$income_group_id=="4"]<- 149999
 inc_abm_13_cpa$lower_bound[inc_abm_13_cpa$income_group_id=="5"]<- 150000
 inc_abm_13_cpa$upper_bound[inc_abm_13_cpa$income_group_id=="5"]<- 349999
 
+inc_abm_13_region$lower_bound[inc_abm_13_region$income_group_id=="1"]<- 0
+inc_abm_13_region$upper_bound[inc_abm_13_region$income_group_id=="1"]<- 29999
+inc_abm_13_region$lower_bound[inc_abm_13_region$income_group_id=="2"]<- 30000
+inc_abm_13_region$upper_bound[inc_abm_13_region$income_group_id=="2"]<- 59999
+inc_abm_13_region$lower_bound[inc_abm_13_region$income_group_id=="3"]<- 60000
+inc_abm_13_region$upper_bound[inc_abm_13_region$income_group_id=="3"]<- 99999
+inc_abm_13_region$lower_bound[inc_abm_13_region$income_group_id=="4"]<- 100000
+inc_abm_13_region$upper_bound[inc_abm_13_region$income_group_id=="4"]<- 149999
+inc_abm_13_region$lower_bound[inc_abm_13_region$income_group_id=="5"]<- 150000
+inc_abm_13_region$upper_bound[inc_abm_13_region$income_group_id=="5"]<- 349999
+
 #calculate the interval with for median income calculation
 #LH added one to interval calculation because it looks like there is a rounding thing happening in SQL script results - keep or delete?
 inc_abm_13_jur$interval_width<-inc_abm_13_jur$upper_bound-inc_abm_13_jur$lower_bound +1
 inc_abm_13_cpa$interval_width<-inc_abm_13_cpa$upper_bound-inc_abm_13_cpa$lower_bound +1
+inc_abm_13_region$interval_width<-inc_abm_13_region$upper_bound-inc_abm_13_region$lower_bound +1
 
 inc_abm_13_jur<- inc_abm_13_jur[order(inc_abm_13_jur$jurisdiction_2015,inc_abm_13_jur$yr,inc_abm_13_jur$income_group_id),]
 inc_abm_13_cpa<- inc_abm_13_cpa[order(inc_abm_13_cpa$cpa_13,inc_abm_13_cpa$yr,inc_abm_13_cpa$income_group_id),]
+inc_abm_13_region<- inc_abm_13_region[order(inc_abm_13_region$yr,inc_abm_13_region$income_group_id),]
+
 head(inc_abm_13_jur,15)
 head(inc_abm_13_cpa,15)
+head(inc_abm_13_region)
 
 #test case: north park cpa
 #cpa_1428<-subset(inc_abm_13_cpa, inc_abm_13_cpa$cpa_13==1428)
