8097aecbbb123b11d5ca1e0d6b763fd7fd790588
lisbethhow <lisbeth.howard@sandag.org>
2019-02-14 10:43:02 -0800

    move location of r script

---
+++ Estimates/Rscripts/hh_hhp_hhs_ds_id.R
@@ -0,0 +1,42 @@
+#HH estimate script
+#started 2/12/2019
+
+
+pkgTest <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dep = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+  
+  
+}
+packages <- c("data.table", "ggplot2", "scales", "sqldf", "rstudioapi", "RODBC", "dplyr", "reshape2", 
+              "stringr","gridExtra","grid","lattice")
+pkgTest(packages)
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+source("../Queries/readSQL.R")
+
+
+getwd()
+options(stringsAsFactors=FALSE)
+
+ds_id=25
+
+datasource_id=25
+
+channel <- odbcDriverConnect('driver={SQL Server}; server=sql2014a8; database=demographic_warehouse; trusted_connection=true')
+hh_sql = getSQL("../Queries/hh_hhp_hhs_ds_id.sql")
+hh<-sqlQuery(channel,hh_sql)
+odbcClose(channel)
+
+dof<-read.csv('M:\\Technical Services\\QA Documents\\Projects\\Estimates\\Data Files\\DOF\\E-5_DOF_formatted.csv')
+
+setnames(dof, 1, "Geography")      
+
+dof$Geography[dof$Geography=="Balance of County"]<- "Unincorporated"
+
+
+
+
+
