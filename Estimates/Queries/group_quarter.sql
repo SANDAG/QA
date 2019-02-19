c90a27f702a1f4765f300723754354c3accec955
lisbethhow <lisbeth.howard@sandag.org>
2019-02-19 09:40:54 -0800

    copy queries for estimates

---
+++ Estimates/Queries/group_quarter.sql
@@ -0,0 +1,25 @@
+SELECT
+	population.yr_id
+	,mgra.geotype
+	,mgra.geozone
+	,population.housing_type_id
+	,housing_type.short_name
+	,SUM(population) as pop
+FROM fact.population
+	INNER JOIN dim.mgra
+	ON mgra.mgra_id = population.mgra_id
+	AND mgra.geotype IN ('jurisdiction', 'region', 'cpa')
+		INNER JOIN dim.housing_type
+		ON housing_type.housing_type_id = population.housing_type_id
+WHERE population.datasource_id = ds_id
+GROUP BY 
+	population.yr_id
+	,mgra.geotype
+	,mgra.geozone
+	,population.housing_type_id
+	,housing_type.short_name
+ORDER BY 
+	population.yr_id
+	,mgra.geotype
+	,mgra.geozone
+	,population.housing_type_id
