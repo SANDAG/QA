20b5f5f555966136d771a5b22fc50b03b12447e2
lisbethhow <lisbeth.howard@sandag.org>
2019-02-22 18:41:20 -0800

    revise script

--- Estimates/Rscripts/hh_hhp_hhs_ds_id.R
+++ Estimates/Rscripts/hh_hhp_hhs_ds_id.R
@@ -23,12 +23,16 @@ options(stringsAsFactors=FALSE)
 
 ds_id=25
 
-datasource_id=25
-
 channel <- odbcDriverConnect('driver={SQL Server}; server=sql2014a8; database=demographic_warehouse; trusted_connection=true')
 hh_sql = getSQL("../Queries/hh_hhp_hhs_ds_id.sql")
 hh_sql <- gsub("ds_id", ds_id,hh_sql)
 hh<-sqlQuery(channel,hh_sql)
+gq_sql = getSQL("../Queries/group_quarter.sql")
+gq_sql <- gsub("ds_id", ds_id,gq_sql)
+gq<-sqlQuery(channel,gq_sql)
+totpop_sql = getSQL("../Queries/total_population.sql")
+totpop_sql <- gsub("ds_id", ds_id,totpop_sql)
+totpop<-sqlQuery(channel,totpop_sql)
 odbcClose(channel)
 
 dof<-read.csv('M:\\Technical Services\\QA Documents\\Projects\\Estimates\\Data Files\\DOF\\E-5_DOF_formatted.csv')
@@ -36,6 +40,9 @@ dof<-read.csv('M:\\Technical Services\\QA Documents\\Projects\\Estimates\\Data F
 setnames(dof, 1, "Geography")      
 
 dof$Geography[dof$Geography=="Balance of County"]<- "Unincorporated"
+
+head(hh)
+
 
 
 
