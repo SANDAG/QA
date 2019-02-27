59b4c580d3b8c9a4a3b54c320ca2b56d4304a30d
lisbethhow <lisbeth.howard@sandag.org>
2019-02-26 16:28:23 -0800

    revise queries to access mgra_denormalize - group quarter not done yet

--- Estimates/Queries/group_quarter.sql
+++ Estimates/Queries/group_quarter.sql
@@ -1,25 +1,23 @@
 SELECT
 	population.yr_id
-	,mgra.geotype
-	,mgra.geozone
-	,population.housing_type_id
+	,'jurisdiction' as geotype
+	,mgra_denormalize.jurisdiction as geozone
+    ,population.housing_type_id
 	,housing_type.short_name
 	,SUM(population) as pop
 FROM fact.population
-	INNER JOIN dim.mgra
-	ON mgra.mgra_id = population.mgra_id
-	AND mgra.geotype IN ('jurisdiction', 'region', 'cpa')
+	INNER JOIN dim.mgra_denormalize
+	ON mgra_denormalize.mgra_id = population.mgra_id
 		INNER JOIN dim.housing_type
 		ON housing_type.housing_type_id = population.housing_type_id
-WHERE population.datasource_id = ds_id
+WHERE population.datasource_id = 26
 GROUP BY 
 	population.yr_id
-	,mgra.geotype
-	,mgra.geozone
+	,mgra_denormalize.jurisdiction
 	,population.housing_type_id
 	,housing_type.short_name
 ORDER BY 
-	population.yr_id
-	,mgra.geotype
-	,mgra.geozone
+	geotype
+	,geozone
+	,population.yr_id
 	,population.housing_type_id
--- Estimates/Queries/hhinc.sql
+++ Estimates/Queries/hhinc.sql
@@ -1,22 +1,76 @@
-SELECT 
+(SELECT 
 	household_income.yr_id
-	,mgra.geotype
-	,mgra.geozone
-	,household_income.income_group_id
+	,'jurisdiction' as geotype
+	,mgra_denormalize.jurisdiction as geozone
+    ,household_income.income_group_id
 	,income_group.name
 	,SUM(household_income.households) as hh
 FROM fact.household_income
-	INNER JOIN dim.mgra
-	ON mgra.geotype IN ('jurisdiction', 'cpa', 'region')
-	AND mgra.mgra_id = household_income.mgra_id
-		INNER JOIN dim.income_group
+	 INNER JOIN dim.mgra_denormalize
+                ON mgra_denormalize.mgra_id = household_income.mgra_id	
+	INNER JOIN dim.income_group
 		ON income_group.income_group_id = household_income.income_group_id
 WHERE household_income.datasource_id = ds_id
 GROUP BY 
 	household_income.yr_id
-	,mgra.geotype
-	,mgra.geozone
-	,household_income.income_group_id
+	,mgra_denormalize.jurisdiction
+    ,household_income.income_group_id
+	,income_group.name)
+UNION
+(SELECT 
+	household_income.yr_id
+	,'cpa' as geotype
+	,mgra_denormalize.cpa as geozone
+    ,household_income.income_group_id
 	,income_group.name
+	,SUM(household_income.households) as hh
+FROM fact.household_income
+	 INNER JOIN dim.mgra_denormalize
+                ON mgra_denormalize.mgra_id = household_income.mgra_id	
+	INNER JOIN dim.income_group
+		ON income_group.income_group_id = household_income.income_group_id
+WHERE household_income.datasource_id = ds_id
+GROUP BY 
+	household_income.yr_id
+	,mgra_denormalize.cpa
+    ,household_income.income_group_id
+	,income_group.name)
+UNION
+(SELECT 
+	household_income.yr_id
+	,'region' as geotype
+	,mgra_denormalize.region as geozone
+    ,household_income.income_group_id
+	,income_group.name
+	,SUM(household_income.households) as hh
+FROM fact.household_income
+	 INNER JOIN dim.mgra_denormalize
+                ON mgra_denormalize.mgra_id = household_income.mgra_id	
+	INNER JOIN dim.income_group
+		ON income_group.income_group_id = household_income.income_group_id
+WHERE household_income.datasource_id = ds_id
+GROUP BY 
+	household_income.yr_id
+	,mgra_denormalize.region
+    ,household_income.income_group_id
+	,income_group.name)
+UNION
+(SELECT 
+	household_income.yr_id
+	,'tract' as geotype
+	,mgra_denormalize.tract as geozone
+    ,household_income.income_group_id
+	,income_group.name
+	,SUM(household_income.households) as hh
+FROM fact.household_income
+	 INNER JOIN dim.mgra_denormalize
+                ON mgra_denormalize.mgra_id = household_income.mgra_id	
+	INNER JOIN dim.income_group
+		ON income_group.income_group_id = household_income.income_group_id
+WHERE household_income.datasource_id = ds_id
+GROUP BY 
+	household_income.yr_id
+	,mgra_denormalize.tract
+    ,household_income.income_group_id
+	,income_group.name)
 ORDER BY 1,2,3,4
-
