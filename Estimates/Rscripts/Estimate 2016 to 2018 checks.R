5a911d8f85a4043cfd5b284e13c12b824e54150e
lisbethhow <lisbeth.howard@sandag.org>
2019-04-09 17:35:52 -0700

    add vacancy script and delete out unneeded info

--- Estimates/Rscripts/Estimate 2016 to 2018 checks.R
+++ Estimates/Rscripts/Estimate 2016 to 2018 checks.R
@@ -40,23 +40,23 @@ gq_24<-sqlQuery(channel,gq_sql)
 totpop_sql = getSQL("../Queries/total_population.sql")
 totpop_sql <- gsub("ds_id", ds_id,totpop_sql)
 totpop_24<-sqlQuery(channel,totpop_sql)
-vac_sql = getSQL("../Queries/vacancy.sql")
-vac_sql <- gsub("ds_id", ds_id,vac_sql)
-vac_24<-sqlQuery(channel,vac_sql)
+# vac_sql = getSQL("../Queries/vacancy.sql")
+# vac_sql <- gsub("ds_id", ds_id,vac_sql)
+# vac_24<-sqlQuery(channel,vac_sql)
 hhinc_sql = getSQL("../Queries/hhinc.sql")
 hhinc_sql <- gsub("ds_id", ds_id,hhinc_sql)
 hhinc_24<-sqlQuery(channel,hhinc_sql)
-demo_sql = getSQL("../Queries/age_ethn_gender.sql")
-demo_sql <- gsub("ds_id", ds_id,demo_sql)
-demo_24<-sqlQuery(channel,demo_sql)
-ma_jur_sql = getSQL("../Queries/median_age_jur.sql")
-ma_jur_24<-sqlQuery(channel,ma_jur_sql)
-ma_reg_sql = getSQL("../Queries/median_age_reg.sql")
-ma_reg_24<-sqlQuery(channel,ma_reg_sql)
-ma_cpa_sql = getSQL("../Queries/median_age_cpa.sql")
-ma_cpa_24<-sqlQuery(channel,ma_cpa_sql)
-ma_tract_sql = getSQL("../Queries/median_age_tract.sql")
-ma_tract_24<-sqlQuery(channel,ma_tract_sql)
+# demo_sql = getSQL("../Queries/age_ethn_gender.sql")
+# demo_sql <- gsub("ds_id", ds_id,demo_sql)
+# demo_24<-sqlQuery(channel,demo_sql)
+# ma_jur_sql = getSQL("../Queries/median_age_jur.sql")
+# ma_jur_24<-sqlQuery(channel,ma_jur_sql)
+# ma_reg_sql = getSQL("../Queries/median_age_reg.sql")
+# ma_reg_24<-sqlQuery(channel,ma_reg_sql)
+# ma_cpa_sql = getSQL("../Queries/median_age_cpa.sql")
+# ma_cpa_24<-sqlQuery(channel,ma_cpa_sql)
+# ma_tract_sql = getSQL("../Queries/median_age_tract.sql")
+# ma_tract_24<-sqlQuery(channel,ma_tract_sql)
 #ma_f_jur_sql = getSQL("../Queries/median_age_female_jur.sql")
 #ma_f_jur_24<-sqlQuery(channel,ma_f_jur_sql)
 #ma_m_jur_sql = getSQL("../Queries/median_age_male_jur.sql")
@@ -79,27 +79,27 @@ gq<-sqlQuery(channel,gq_sql)
 totpop_sql = getSQL("../Queries/total_population.sql")
 totpop_sql <- gsub("ds_id", ds_id,totpop_sql)
 totpop <-sqlQuery(channel,totpop_sql)
-vac_sql = getSQL("../Queries/vacancy.sql")
-vac_sql <- gsub("ds_id", ds_id,vac_sql)
-vac<-sqlQuery(channel,vac_sql)
+# vac_sql = getSQL("../Queries/vacancy.sql")
+# vac_sql <- gsub("ds_id", ds_id,vac_sql)
+# vac<-sqlQuery(channel,vac_sql)
 hhinc_sql = getSQL("../Queries/hhinc.sql")
 hhinc_sql <- gsub("ds_id", ds_id,hhinc_sql)
 hhinc<-sqlQuery(channel,hhinc_sql)
-demo_sql = getSQL("../Queries/age_ethn_gender.sql")
-demo_sql <- gsub("ds_id", ds_id,demo_sql)
-demo<-sqlQuery(channel,demo_sql)
-ma_jur_sql = getSQL("../Queries/median_age_jur_26.sql")
-ma_jur<-sqlQuery(channel,ma_jur_sql)
-ma_reg_sql = getSQL("../Queries/median_age_reg_26.sql")
-ma_reg<-sqlQuery(channel,ma_reg_sql)
-ma_jur_sql = getSQL("../Queries/median_age_jur_26.sql")
-ma_jur<-sqlQuery(channel,ma_jur_sql)
-ma_reg_sql = getSQL("../Queries/median_age_cpa_26.sql")
-ma_reg<-sqlQuery(channel,ma_reg_sql)
-ma_cpa_sql = getSQL("../Queries/median_age_cpa_26.sql")
-ma_cpa<-sqlQuery(channel,ma_cpa_sql)
-ma_tract_sql = getSQL("../Queries/median_age_tract_26.sql")
-ma_tract<-sqlQuery(channel,ma_tract_sql)
+# demo_sql = getSQL("../Queries/age_ethn_gender.sql")
+# demo_sql <- gsub("ds_id", ds_id,demo_sql)
+# demo<-sqlQuery(channel,demo_sql)
+# ma_jur_sql = getSQL("../Queries/median_age_jur_26.sql")
+# ma_jur<-sqlQuery(channel,ma_jur_sql)
+# ma_reg_sql = getSQL("../Queries/median_age_reg_26.sql")
+# ma_reg<-sqlQuery(channel,ma_reg_sql)
+# ma_jur_sql = getSQL("../Queries/median_age_jur_26.sql")
+# ma_jur<-sqlQuery(channel,ma_jur_sql)
+# ma_reg_sql = getSQL("../Queries/median_age_cpa_26.sql")
+# ma_reg<-sqlQuery(channel,ma_reg_sql)
+# ma_cpa_sql = getSQL("../Queries/median_age_cpa_26.sql")
+# ma_cpa<-sqlQuery(channel,ma_cpa_sql)
+# ma_tract_sql = getSQL("../Queries/median_age_tract_26.sql")
+# ma_tract<-sqlQuery(channel,ma_tract_sql)
 #ma_f_jur_sql = getSQL("../Queries/median_age_female_jur_26.sql")
 #ma_f_jur<-sqlQuery(channel,ma_f_jur_sql)
 #ma_m_jur_sql = getSQL("../Queries/median_age_male_jur_26.sql")
@@ -125,15 +125,17 @@ setnames(gq_24, old="pop", new="gqpop_24")
 #hutot <- aggregate(units~yr_id + geotype + geozone, data=vac, sum)
 #setnames(hutot, old = "units", new = "hu")
 
-hu_24 <- dcast(vac_24, yr_id + geotype + geozone ~ short_name, value.var="units")
-hutot_24 <- aggregate(units~yr_id + geotype + geozone, data=vac_24, sum)
+#hu_24 <- dcast(vac_24, yr_id + geotype + geozone ~ short_name, value.var="units")
+#hutot_24 <- aggregate(units~yr_id + geotype + geozone, data=vac_24, sum)
 
 #merge sandag data and calculate the vacancy rate
+
+
 #I think I can remove vac file since I have hu and hh
 #clean up geozone for merge
-vac$geozone <- gsub("\\*","",vac$geozone)
-vac$geozone <- gsub("\\-","_",vac$geozone)
-vac$geozone <- gsub("\\:","_",vac$geozone)
+# vac$geozone <- gsub("\\*","",vac$geozone)
+# vac$geozone <- gsub("\\-","_",vac$geozone)
+# vac$geozone <- gsub("\\:","_",vac$geozone)
 totpop$geozone <- gsub("\\*","",totpop$geozone)
 totpop$geozone <- gsub("\\-","_",totpop$geozone)
 totpop$geozone <- gsub("\\:","_",totpop$geozone)
@@ -150,9 +152,9 @@ hh$geozone <- gsub("\\:","_",hh$geozone)
 #hu$geozone <- gsub("\\-","_",hu$geozone)
 #hu$geozone <- gsub("\\:","_",hu$geozone)
 
-vac_24$geozone <- gsub("\\*","",vac_24$geozone)
-vac_24$geozone <- gsub("\\-","_",vac_24$geozone)
-vac_24$geozone <- gsub("\\:","_",vac_24$geozone)
+# vac_24$geozone <- gsub("\\*","",vac_24$geozone)
+# vac_24$geozone <- gsub("\\-","_",vac_24$geozone)
+# vac_24$geozone <- gsub("\\:","_",vac_24$geozone)
 totpop_24$geozone <- gsub("\\*","",totpop_24$geozone)
 totpop_24$geozone <- gsub("\\-","_",totpop_24$geozone)
 totpop_24$geozone <- gsub("\\:","_",totpop_24$geozone)
@@ -162,31 +164,40 @@ gq_24$geozone <- gsub("\\:","_",gq_24$geozone)
 hh_24$geozone <- gsub("\\*","",hh_24$geozone)
 hh_24$geozone <- gsub("\\-","_",hh_24$geozone)
 hh_24$geozone <- gsub("\\:","_",hh_24$geozone)
-hutot_24$geozone <- gsub("\\*","",hutot_24$geozone)
-hutot_24$geozone <- gsub("\\-","_",hutot_24$geozone)
-hutot_24$geozone <- gsub("\\:","_",hutot_24$geozone)
-hu_24$geozone <- gsub("\\*","",hu_24$geozone)
-hu_24$geozone <- gsub("\\-","_",hu_24$geozone)
-hu_24$geozone <- gsub("\\:","_",hu_24$geozone)
+# hutot_24$geozone <- gsub("\\*","",hutot_24$geozone)
+# hutot_24$geozone <- gsub("\\-","_",hutot_24$geozone)
+# hutot_24$geozone <- gsub("\\:","_",hutot_24$geozone)
+# hu_24$geozone <- gsub("\\*","",hu_24$geozone)
+# hu_24$geozone <- gsub("\\-","_",hu_24$geozone)
+# hu_24$geozone <- gsub("\\:","_",hu_24$geozone)
 
 
 
 head(totpop)
 
+#merge data and calculate the vacancy rate
 est <- merge(totpop, gq, by.x = c("yr_id", "geotype", "geozone"), by.y = c("yr_id", "geotype", "geozone"), all=TRUE)
 est <- merge(est, hh, by.x = c("yr_id", "geotype", "geozone"), by.y = c("yr_id", "geotype", "geozone"), all=TRUE)
 #est <- merge(est, hutot, by.x = c("yr_id", "geotype", "geozone"), by.y = c("yr_id", "geotype", "geozone"), all=TRUE)
 #est <- merge(est, hu, by.x = c("yr_id", "geotype", "geozone"), by.y = c("yr_id", "geotype", "geozone"), all=TRUE)
-
 setnames(est, old="units",new="hu")
+#calculate vac rate
+est$vac <- ((est$hu-est$households)/est$hu)*100
+est$vac <- round(est$vac,digits = 2)
+
 head(est)
 
 est_24 <- merge(totpop_24, gq_24, by.x = c("yr_id", "geotype", "geozone"), by.y = c("yr_id", "geotype", "geozone"), all=TRUE)
 est_24 <- merge(est_24, hh_24, by.x = c("yr_id", "geotype", "geozone"), by.y = c("yr_id", "geotype", "geozone"), all=TRUE)
-est_24 <- merge(est_24, hutot_24, by.x = c("yr_id", "geotype", "geozone"), by.y = c("yr_id", "geotype", "geozone"), all=TRUE)
-est_24 <- merge(est_24, hu_24, by.x = c("yr_id", "geotype", "geozone"), by.y = c("yr_id", "geotype", "geozone"), all=TRUE)
+#est_24 <- merge(est_24, hutot_24, by.x = c("yr_id", "geotype", "geozone"), by.y = c("yr_id", "geotype", "geozone"), all=TRUE)
+#est_24 <- merge(est_24, hu_24, by.x = c("yr_id", "geotype", "geozone"), by.y = c("yr_id", "geotype", "geozone"), all=TRUE)
+setnames(est_24, old="units",new="hu")
+#calculate vac rate
+est_24$vac <- ((est_24$hu-est_24$households)/est_24$hu)*100
+est_24$vac <- round(est_24$vac,digits = 2)
 
 head(est_24)
+
 
 #change integer to numeric type
 est$pop <- as.numeric(est$pop)
@@ -203,11 +214,11 @@ est_24$pop <- as.numeric(est_24$pop)
 est_24$gqpop <- as.numeric(est_24$gqpop)
 est_24$households <- as.numeric(est_24$households)
 est_24$hhp <- as.numeric(est_24$hhp)
-est_24$units <- as.numeric(est_24$units.y)
-est_24$mf <- as.numeric(est_24$mf)
-est_24$mh <- as.numeric(est_24$mh)
-est_24$sfmu <- as.numeric(est_24$sfmu)
-est_24$sf <- as.numeric(est_24$sf)
+est_24$hu <- as.numeric(est_24$hu)
+# est_24$mf <- as.numeric(est_24$mf)
+# est_24$mh <- as.numeric(est_24$mh)
+# est_24$sfmu <- as.numeric(est_24$sfmu)
+# est_24$sf <- as.numeric(est_24$sf)
 
 head(est_24)
 
@@ -216,9 +227,8 @@ est$geozone[est$geotype=='region'] <- 'San Diego Region'
 est_24$geozone[est_24$geotype=='region'] <- 'San Diego Region'
 
          
-setnames(est_24, old=c("pop","gqpop_24","households","hhp","hhs","mf","mh","sfmu","sf","units"), new=c("pop_24","gqpop_24","households_24",
-                                                                                                 "hhp_24","hhs_24","mf_24","mh_24",
-                                                                                                 "sfa_24","sfd_24","hu_24"))
+setnames(est_24, old=c("pop","gqpop_24","households","hhp","hhs", "hu","vac"), new=c("pop_24","gqpop_24","households_24",
+                                                                                                 "hhp_24","hhs_24","hu_24","vac_24"))
 
 est$geozone <- gsub("^\\s+|\\s+$", "", est$geozone)
 est_24$geozone <- gsub("^\\s+|\\s+$", "", est_24$geozone)
@@ -231,9 +241,6 @@ table(est_24_27$geozone)
 table(est_24_27$geotype.x)
 table(est_24_27$geotype.y)
 
-##est_24_27$tot_pop_diff <- rowSums(est_24_27 [,c(est_24_27$pop_est, est_24_27$pop_est_24)], na.rm=TRUE)
-##est_24_27$tot_pop_diff <- rowSums(est_24_27[,c(pop_est, pop_est_24)], na.rm=TRUE)
-
 colnames(est_24_27)
 
 #calculate number change
@@ -242,11 +249,12 @@ est_24_27$hhp_numchg <- est_24_27$hhp-est_24_27$hhp_24
 est_24_27$gqpop_numchg <- est_24_27$gqpop.y-est_24_27$gqpop_24
 est_24_27$hh_numchg <- est_24_27$households-est_24_27$households_24
 est_24_27$hu_numchg <- est_24_27$hu-est_24_27$hu_24
-est_24_27$sfa_numchg <- est_24_27$sfa-est_24_27$sfa_24
-est_24_27$sfd_numchg <- est_24_27$sfd-est_24_27$sfd_24
-est_24_27$mf_numchg <- est_24_27$mf-est_24_27$mf_24
-est_24_27$mh_numchg <- est_24_27$mh-est_24_27$mh_24
+#est_24_27$sfa_numchg <- est_24_27$sfa-est_24_27$sfa_24
+#est_24_27$sfd_numchg <- est_24_27$sfd-est_24_27$sfd_24
+#est_24_27$mf_numchg <- est_24_27$mf-est_24_27$mf_24
+#est_24_27$mh_numchg <- est_24_27$mh-est_24_27$mh_24
 est_24_27$hhs_numchg <- est_24_27$hhs-est_24_27$hhs_24
+est_24_27$vac_numchg <- est_24_27$vac-est_24_27$vac_24
 
 head(subset(est_24_27, est_24_27$geotype.x=="jurisdiction"), 8)
 
@@ -282,7 +290,6 @@ est_24_27 <- est_24_27[order(est_24_27$geozone, est_24_27$yr_id),]
 
 write.csv(est_24_27,"M:\\Technical Services\\QA Documents\\Projects\\Estimates\\4_Data Files\\Est 2016 to 2018 differences2.csv" )
 
-head(est)
 
 #calculate change in ID 27 data
 est <- est[order(est$geotype,est$geozone,est$yr_id),]
@@ -298,9 +305,9 @@ est$hhN_pct<-round(est$hhN_pct,digits=2)
 est$huN_chg <- est$hu - lag(est$hu)
 est$huN_pct <- (est$huN_chg / lag(est$hu))*100
 est$huN_pct<-round(est$huN_pct,digits=2)
-est$vacN_chg <- est$vac - lag(est$vac)
-est$vacN_pct <- (est$vacN_chg / lag(est$vac))*100
-est$vacN_pct<-round(est$vacN_pct,digits=2)
+#est$vacN_chg <- est$vac - lag(est$vac)
+#est$vacN_pct <- (est$vacN_chg / lag(est$vac))*100
+#est$vacN_pct<-round(est$vacN_pct,digits=2)
 
 head(est[est$geotype=="jurisdiction",],10)
 
@@ -313,14 +320,14 @@ est$hhN_pct[est$yr_id==2010] <- NA
 est$hhN_chg[est$yr_id==2010] <- NA 
 est$huN_pct[est$yr_id==2010] <- NA
 est$huN_chg[est$yr_id==2010] <- NA 
-est$vacN_pct[est$yr_id==2010] <- NA
-est$vacN_chg[est$yr_id==2010] <- NA 
+#est$vacN_pct[est$yr_id==2010] <- NA
+#est$vacN_chg[est$yr_id==2010] <- NA 
 #set Inf values to NA for later calculations
 est$hhpN_pct[is.infinite(est$hhpN_pct)] <-NA 
 est$hhsN_pct[is.infinite(est$hhsN_pct)] <-NA 
 est$hhN_pct[is.infinite(est$hhN_pct)] <-NA 
 est$huN_pct[is.infinite(est$huN_pct)] <-NA 
-est$vacN_pct[is.infinite(est$vacN_pct)] <-NA 
+#est$vacN_pct[is.infinite(est$vacN_pct)] <-NA 
 #subset est file for jurisdiction and region
 est_jur <- subset(est, est$geotype=="jurisdiction")
 est_reg <- subset(est, est$geotype=="region")
@@ -334,6 +341,8 @@ est_reg <- subset(est, est$geotype=="region")
 
 rm(hh_24,gq_24,hhinc_24,hu_24,hutot_24,demo_24,ma_tract_24,ma_cpa_24,ma_jur_24,ma_reg_24,totpop_24,vac_24)
 
+#make copy of est in case need to rerun after this point
+est_backup <- data.frame(est) 
 
 est <- subset(est, est$geotype=="tract")
 table(est$geotype)
