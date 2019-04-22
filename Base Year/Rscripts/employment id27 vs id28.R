b50f0b76e8df1a71459843c24394e94009111309
lisbethhow <lisbeth.howard@sandag.org>
2019-04-22 14:26:15 -0700

    update employment base year script to check id28 to id27

--- Base Year/Rscripts/employment id27 vs id28.R
+++ Base Year/Rscripts/employment id27 vs id28.R
@@ -31,13 +31,17 @@ table(geo$cpa,geo$jurisdiction)
 
 #read in mgra files for 2016 and 2012
 mgra_id27<-read.csv('T:\\socioec\\Current_Projects\\XPEF10\\abm_csv\\mgra13_based_input2016_01.csv',stringsAsFactors = FALSE ,fileEncoding="UTF-8-BOM")
-mgra_id28<-read.csv('T:\\socioec\\Current_Projects\\XPEF10\\abm_csv\\mgra13_based_input2016_01.csv',stringsAsFactors = FALSE ,fileEncoding="UTF-8-BOM")
+mgra_id28<-read.csv('T:\\socioec\\Current_Projects\\XPEF11\\abm_csv\\mgra13_based_input2016_01.csv',stringsAsFactors = FALSE ,fileEncoding="UTF-8-BOM")
 
-testtaz <- unique(mgra_id27$taz[mgra_id27$hhp>0])
+#check across datasource ids the unique number of taz and mgra
+testtaz27 <- unique(mgra_id27$taz[mgra_id27$hhp>0])
+testmgra27 <- unique(mgra_id27$mgra[mgra_id27$hhp>0])
+testtaz28 <- unique(mgra_id28$taz[mgra_id28$hhp>0])
+testmgra28 <- unique(mgra_id28$mgra[mgra_id28$hhp>0])
 
-testmgra <- unique(mgra_id27$mgra[mgra_id27$hhp>0])
+#check across datasource ids the total number of jobs
 mgra_employ_id27 <- sum(mgra_id27$emp_total)
-
+mgra_employ_id28 <- sum(mgra_id28$emp_total)
 
 #create civilian employment total for each file
 mgra_id27$civ_emp_tot<-mgra_id27$emp_total-mgra_id27$emp_fed_mil
@@ -77,43 +81,27 @@ mgra_27_28<-merge(mgra_id27_long, mgra_id28_long, by=c("mgra", "variable"))
 #calculate difference from new 2016 to 2012 for each variable
 mgra_27_28$diff<-mgra_27_28$value_id28-mgra_27_28$value_id27
 
-
-
-#script is updated to here
-###########################
-###########################
-###########################
-
-
-
-
-
-
-
-
-
-
 #merge in geography files
-mgra_16_12<-merge(mgra_16_12, geo, by="mgra")
+mgra_27_28<-merge(mgra_27_28, geo, by="mgra")
 
 #check results of merge
-head(mgra_16_12[mgra_16_12$jurisdiction==1,],15)
+head(mgra_27_28[mgra_27_28$jurisdiction==1,],15)
 
 #aggregate totals to jurisdiction
-emp_jur_16_12<-aggregate(cbind(value_2016,value_2012,diff)~variable+jurisdiction_name, data=mgra_16_12, sum)
+emp_jur_27_28<-aggregate(cbind(value_id28,value_id27,diff)~variable+jurisdiction_name, data=mgra_27_28, sum)
 
-head(emp_jur_16_12)
 
 #aggregate the totals by cpa
-emp_cpa_16_12 <- subset(mgra_16_12,mgra_16_12$cpa!="not_cpa")
-emp_cpa_16_12<-aggregate(cbind(value_2016,value_2012,diff)~variable+jurisdiction_and_cpa_name, data=emp_cpa_16_12, sum)
+emp_cpa_27_28 <- subset(mgra_27_28,mgra_27_28$cpa!="not_cpa")
+emp_cpa_27_28<-aggregate(cbind(value_id28,value_id27,diff)~variable+jurisdiction_and_cpa_name, data=emp_cpa_27_28, sum)
 
 #aggregate for region totals
-emp_reg_16_12<-aggregate(cbind(value_2016,value_2012,diff)~variable, data=mgra_16_12, sum)
+emp_reg_27_28<-aggregate(cbind(value_id28,value_id27,diff)~variable, data=mgra_27_28, sum)
 
-write.csv(emp_reg_16_12, "M:\\Technical Services\\QA Documents\\Projects\\Base Year_Popsyn\\4_Data Files\\ouput files\\emp_reg_16_12.csv" )
+#write files for each geography
+write.csv(emp_reg_27_28, "M:\\Technical Services\\QA Documents\\Projects\\Base Year_Popsyn\\4_Data Files\\ouput files\\emp_reg_27_28.csv" )
 
-write.csv(emp_jur_16_12, "M:\\Technical Services\\QA Documents\\Projects\\Base Year_Popsyn\\4_Data Files\\ouput files\\emp_jur_16_12.csv" )
+write.csv(emp_jur_27_28, "M:\\Technical Services\\QA Documents\\Projects\\Base Year_Popsyn\\4_Data Files\\ouput files\\emp_jur_27_28.csv" )
 
-write.csv(emp_cpa_16_12, "M:\\Technical Services\\QA Documents\\Projects\\Base Year_Popsyn\\4_Data Files\\ouput files\\emp_cpa_16_12.csv" )
+write.csv(emp_cpa_27_28, "M:\\Technical Services\\QA Documents\\Projects\\Base Year_Popsyn\\4_Data Files\\ouput files\\emp_cpa_27_28.csv" )
 
