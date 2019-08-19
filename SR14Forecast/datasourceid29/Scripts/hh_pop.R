1a8a6d9566b9efb90fbb835e41a109eb2f31df9c
lisbethhow <lisbeth.howard@sandag.org>
2019-08-19 14:21:16 -0700

    revise load process, test Dave's parameters, revise script for facet of total pop to divide large and small jurisdictions

--- SR14Forecast/datasourceid29/Scripts/hh_pop.R
+++ SR14Forecast/datasourceid29/Scripts/hh_pop.R
@@ -1,74 +1,61 @@
 #forecast id=29.
-#hhpop change
-
-#fix line 26
+#hhpop plots change
 
 
-#forecast 2021 household pop plots
+maindir = dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(maindir)
 
+source("config.R")
+source("../Queries/readSQL.R")
+source("common_functions.R")
 
-pkgTest <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg))
-    install.packages(new.pkg, dep = TRUE)
-  sapply(pkg, require, character.only = TRUE)
-  
-  
-}
 packages <- c("data.table", "ggplot2", "scales", "sqldf", "rstudioapi", "RODBC", "plyr", "dplyr", "reshape2", 
               "stringr","gridExtra","grid","lattice", "gtable")
 pkgTest(packages)
 
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-source("../Queries/readSQL.R")
+###setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+###source("../Queries/readSQL.R")
 
-ds_id = 29
+###ds_id = 29
 
 channel <- odbcDriverConnect('driver={SQL Server}; server=sql2014a8; database=demographic_warehouse; trusted_connection=true')
-# hh_sql = getSQL("C:/Users/jyen/Documents/QA/SR14Forecast/Queries/hh_hhp_hhs_ds_id.sql")
-hh_sql = getSQL("../Queries/hh_hhp_hhs_ds_id.sql")
-hh_sql <- gsub("ds_id", ds_id, hh_sql)
-hh<-sqlQuery(channel,hh_sql)
-odbcClose(channel)
+###hh_sql = getSQL("C:/Users/lho/Documents/QA/SR14Forecast/Queries/hh_hhp_hhs_ds_id.sql")
+###hh_sql = getSQL("../Queries/hh_hhp_hhs_ds_id.sql")
+###hh_sql <- gsub("ds_id", ds_id, hh_sql)
+###hhp<-sqlQuery(channel,hh_sql)
+###odbcClose(channel)
+
+###table(hhp$yr_id)
+
 
 
 
 #create dataframes
-# hhp <- data.frame()
-# sourcename <- data.frame()
-# hh <- data.frame()
+ hhp <- data.frame()
+ sourcename <- data.frame()
+ hh <- data.frame()
 
 #work with AK to fix code
-# for(i in 1:length(datasource_ids)) {
-#   # get the name of the datasource
-#   datasource_name <- readDB("../Queries/datasource_name.sql",datasource_ids[i])
-#   sourcename <- rbind(sourcename,datasource_name)
-# 
-#   # get hhpop data
-#   hh <- readDB("../Queries/hh_hhp_hhs_ds_id.sql",datasource_ids[i])
-#   hh$datasource_id = datasource_ids[i]
-#   hh$series = datasource_names[i]
-#   hhp <- rbind(hhp,hh)
-# }
+ for(i in 1:length(datasource_ids)) {
+   # get the name of the datasource
+   datasource_name <- readDB("../Queries/datasource_name.sql",datasource_ids[i])
+   sourcename <- rbind(sourcename,datasource_name)
+ 
+   # get hhpop data
+   hh <- readDB("../Queries/hh_hhp_hhs_ds_id.sql",datasource_ids[i])
+   hh$datasource_id = datasource_ids[i]
+   hh$series = datasource_names[i]
+   hhp <- rbind(hhp,hh)
+ }
 
 
 
-# #config contents
+# #config contents - For reference only
 # datasource_id_current <- 28
 # datasource_ids <- c(13,28)
 # datasource_names <- c("Series 13 (ds 13)","Series 14 (ds 28)")
 # datasource_name_short <- c("Series 13","Series 14")
 # datasource_outfolder <- "vacancyds28"
-
-#get the name of the datasource
-  datasource_name <- readDB("../Queries/datasource_name.sql")
-#  sourcename <- rbind(sourcename,datasource_name)
-
-  # get hhpop data
-  hh <- readDB("../Queries/hh_hhp_hhs_ds_id.sql",datasource_ids[i])
-  hh$datasource_id = datasource_ids[i]
-  hh$series = datasource_names[i]
-  hhp <- rbind(hhp,hh)
 
 
 # get cpa id
@@ -76,16 +63,19 @@ geo_id <- readDB("../Queries/get_cpa_and_jurisdiction_id.sql",datasource_ids[i])
 odbcClose(channel)
 
 # fix names of sr13 cpas to match sr14 cpas
-hhp <- rename_sr13_cpas(hhp)
+#hhp <- rename_sr13_cpas(hhp)
+
+hhp <- subset(hhp, yr_id==2016 | yr_id==2018 | yr_id==2020 | yr_id==2025 | yr_id==2030 | yr_id==2035 | yr_id==2040 | yr_id==2045 | yr_id==2050)
 
 head(hhp)
 hhp <- hhp[order(hhp$geotype,hhp$geozone,hhp$yr_id),]
 hhp$N_chg <- ave(hhp$hhp, factor(hhp$geozone), FUN=function(x) c(NA,diff(x)))
-hhp$N_pct <- (hhp$N_chg / lag(hh$hhp))*100
+hhp$N_pct <- (hhp$N_chg / lag(hhp$hhp))*100
 hhp$N_pct <- round(hhp$N_pct, digits = 2)
 
-hhp$N_chg[hhp$yr_id == 2018] <- 0
-hhp$N_pct[hhp$yr_id == 2018] <- 0
+hhp$N_chg[hhp$yr_id == 2016] <- 0
+hhp$N_pct[hhp$yr_id == 2016] <- 0
+hhp$N_pct[is.nan(hhp$N_pct)] <- 0
 
 hhp_jur = subset(hhp,geotype=='jurisdiction')
 colnames(hhp_jur)[colnames(hhp_jur)=="geozone"] <- "cityname"
@@ -105,6 +95,18 @@ hhp_cpa$regN<-hhp_region[match(hhp_cpa$yr_id, hhp_region$yr_id),"hhp"]
 hhp_jur$regN_pct<-hhp_region[match(hhp_jur$yr_id, hhp_region$yr_id),"N_pct"]
 hhp_cpa$regN_pct<-hhp_region[match(hhp_cpa$yr_id, hhp_region$yr_id),"N_pct"]
 
+##test Dave's qa parameters
+
+#create subset of cases
+hhp_change <- subset(hhp, N_chg>7500 & N_pct>20.0)
+#create unique list of geozone that are within parameters
+hhp_chg_geos <- unique(hhp_change$geozone)
+#create dataset with all records for geozone within parameters
+hhp_out_of_range <- hhp [hhp$geozone %in% hhp_chg_geos,]
+
+write.csv(hhp_out_of_range,"M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\Results\\Phase 8\\hhp_out_of_range.csv", row.names=FALSE)
+
+
 
 maindir = dirname(rstudioapi::getSourceEditorContext()$path)
 results<-"plots\\hh_pop\\Jur\\"
@@ -118,8 +120,6 @@ hhp_jur$N <-  hhp_jur$hhp
 
 jur_list = unique(hhp_jur[["cityname"]])
 jur_list2 = unique(hhp_jur[["cityname"]])
-
-
 
 for(i in jur_list) { #1:length(unique(hhp_jur[["cityname"]]))){
   plotdat = subset(hhp_jur, hhp_jur$cityname==i)
@@ -164,8 +164,6 @@ for(i in jur_list) { #1:length(unique(hhp_jur[["cityname"]]))){
 
 ####CPA
 
-hhp_cpa$N_pct[is.nan(hhp_cpa$N_pct)] <- 1
-
 hhp_cpa$year<- "y"
 hhp_cpa$yr <- as.factor(paste(hhp_cpa$year, hhp_cpa$yr, sep = ""))
 hhp_cpa$N <-  hhp_cpa$hhpp
@@ -177,8 +175,43 @@ results<-"plots\\hh_pop\\cpa\\"
 ifelse(!dir.exists(file.path(maindir,results)), dir.create(file.path(maindir,results), showWarnings = TRUE, recursive=TRUE),0)
 
 
+###facet of all jurisdiction and region plot lines for hhpop
 
-for(i in cpa_list) { #1:length(unique(hhp_jur[["cityname"]]))){
+
+pop_large <- hhp_jur[hhp_jur$hhp>=100000 & hhp_jur$yr_id==2050,]
+pop_small <- hhp_jur[hhp_jur$hhp<100000 & hhp_jur$yr_id==2050,]
+
+
+hhp_geos_100Kplus <- unique(pop_large$geozone)
+hhp_geos_under100K <- unique(pop_small$geozone)
+
+
+hhp_jur_100Kplus <- hhp_jur[hhp_jur$geozone %in% hhp_geos_100Kplus,]
+hhp_jur_under100K <- hhp_jur[hhp_jur$geozone %in% hhp_geos_under100K,]
+
+
+
+hhp_plot_100Kplus <- rbind(hhp_jur_100Kplus,hhp_region)
+sp<-ggplot(hhp_plot_100Kplus,aes(x=yr_id,y=hhp)) + geom_point(shape=1) + geom_line()
+plus100K <- sp + facet_wrap(~geozone,ncol=3,scales="free")
+
+
+hhp_plot_under100K <- rbind(hhp_jur_under100K,hhp_region)
+sp<-ggplot(hhp_plot_under100K,aes(x=yr_id,y=hhp)) + geom_point(shape=1) + geom_line()
+#under100K <- sp + facet_wrap(~geozone,ncol=3,scales="free")
+sp + facet_wrap(~geozone,ncol=3,scales="free")
+
+
+ggsave(plus100K, file= paste(results, 'total household pop 100K plus', ds_id,".png", sep=''),
+       width=6, height=8, dpi=100)#, scale=2)
+
+ggsave(under100K, file= paste(results, 'total household pop under 100K', ds_id,".png", sep=''),
+       width=6, height=8, dpi=100)#, scale=2)
+
+
+##cpa ggplot
+
+for(i in cpa_list) { #1:length(unique(hhp_cpa[["cpaname"]]))){
   plotdat = subset(hhp_cpa, hhp_cpa$cpaname==i)
   ravg = max(plotdat$reg,na.rm=TRUE)/max(plotdat$N_chg,na.rm=TRUE)
   ravg[which(!is.finite(ravg))] <- 0
@@ -217,18 +250,6 @@ for(i in cpa_list) { #1:length(unique(hhp_jur[["cityname"]]))){
   ggsave(output, file= paste(results, 'total household pop', i, ds_id,".png", sep=''),
          width=6, height=8, dpi=100)#, scale=2)
 }
-
-
-
-###Per Anne's request
-hhp_jur = subset(hhp,geotype=='jurisdiction')
-hhp_region = subset(hhp,geotype=='region')
-hhp_region$geozone = 'Region'
-hhp_plot <- rbind(hhp_jur,hhp_region)
-sp<-ggplot(hhp_plot,aes(x=yr_id,y=hhp)) + geom_point(shape=1) + geom_line()
-sp + facet_wrap(~geozone,ncol=3,scales="free")
-
-
 
 
 
