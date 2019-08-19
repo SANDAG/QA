73b10020aa16269e5bb44a7865d24e234a7d7ca7
SANDAGNET\aku <aku@sandag.org>
2019-08-19 12:44:43 -0700

    add vacancy scripts to datasourceid29

---
+++ SR14Forecast/datasourceid29/Queries/vacancy_ds_id.sql
@@ -0,0 +1,31 @@
+
+USE demographic_warehouse;
+
+SELECT
+	housing.yr_id
+	,housing.datasource_id
+	,mgra.geotype 
+	,mgra.geozone
+	,SUM(housing.unoccupiable) as unoccupiable
+	,SUM(housing.occupied) as hh
+	,SUM(housing.units) as units
+	,CASE
+		  WHEN (SUM(housing.units))=0 THEN 0 
+		  ELSE round(1 - SUM(housing.occupied)/CAST(SUM(housing.units) as float),4) 
+	END as vacancy_rate
+FROM fact.housing
+	INNER JOIN dim.mgra
+	ON mgra.mgra_id = housing.mgra_id
+	AND mgra.geotype IN ('jurisdiction', 'region', 'cpa')
+WHERE housing.datasource_id IN (ds_id) AND housing.yr_id NOT IN (2023,2026,2029,2032)
+GROUP BY 
+	housing.yr_id
+	,mgra.geotype
+	,mgra.geozone
+	,housing.datasource_id
+order by geotype,geozone,yr_id,datasource_id
+
+-- total rows
+--864 rows
+--SELECT (87 + 1 + 1 + 19) * 8	
+--note 87 cpas + 1 not in a cpa + 1 region + 19 (18 jurisdictions + unincorporated) x 8 increments
--- SR14Forecast/datasourceid29/Scripts/config.R
+++ SR14Forecast/datasourceid29/Scripts/config.R
@@ -1,13 +1,16 @@
-#vacancy config
+
 #LH added datasource_id_current
 
 datasource_id_current <- 28
-datasource_ids <- c(13,28)
-datasource_names <- c("Series 13 (ds 13)","Series 14 (ds 28)")
-datasource_name_short <- c("Series 13","Series 14")
-datasource_outfolder <- "vacancyds28"
-#
+
+
 # datasource_ids <- c(17,28)
 # datasource_names <- c("Series 14 (ds 17)","Series 14 (ds 28)")
 # datasource_name_short <- c("Series 14","Series 14")
-# datasource_outfolder <- "vacancy_ds17_and_ds28"
+# datasource_outfolder <- "vacancy"
+
+datasource_ids <- c(13,29)
+datasource_names <- c("Series 13 (ds 13)","Series 14 (ds 29)")
+datasource_name_short <- c("Series 13","Series 14")
+datasource_outfolder <- "vacancy"
+#
