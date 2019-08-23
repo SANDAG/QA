78c7ebc0b303a2302a9411e6944cea1a26f38646
SANDAGNET\aku <aku@sandag.org>
2019-08-23 09:54:08 -0700

    create spreadsheet for test results requested by EDAM

---
+++ SR14Forecast/datasourceid29/Scripts/functions_for_percent_change.R
@@ -0,0 +1,80 @@
+
+calculate_pct_chg <- function(df, edam_var) {
+  edam_var <- enquo(edam_var)
+  df1 <- df %>% select("datasource_id","yr_id","geotype","geozone",!!edam_var)
+  #change over increments 
+  df1 <- df1 %>% 
+    group_by(geozone,geotype) %>% 
+    mutate(change = !!edam_var - lag(!!edam_var))
+  #percent change over increments
+  df1 <- df1 %>%
+    group_by(geozone,geotype) %>%  # avoid divide by zero with ifelse
+    mutate(percent_change = ifelse(lag(!!edam_var)==0, NA, (!!edam_var - lag(!!edam_var))/lag(!!edam_var)))
+  df1$percent_change <- round(df1$percent_change, digits = 2)
+  #average annual change
+  df1 <- df1 %>% 
+    group_by(geozone,geotype) %>% 
+    mutate(average_annual_change = (!!edam_var - lag(!!edam_var))/(yr_id - lag(yr_id)) )
+  df1$average_annual_change  <- round(df1$average_annual_change, digits = 0)
+  #average annual percent change
+  df1 <- df1 %>%
+    group_by(geozone,geotype) %>%  # avoid divide by zero with ifelse
+    mutate(avg_ann_pct_chg = ifelse(lag(!!edam_var)==0, NA, 
+                                    (!!edam_var - lag(!!edam_var))/lag(!!edam_var)/(yr_id - lag(yr_id))))
+  df1$avg_ann_pct_chg <- round(df1$avg_ann_pct_chg, digits = 2)
+  return(df1)
+}
+
+calculate_pass_fail <- function(df, cutoff1,cutoff2) {
+  df <- df %>%
+    mutate(pass.or.fail = case_when(average_annual_change >= cutoff1 & avg_ann_pct_chg >= cutoff2 ~ "fail",
+                                    change >= cutoff1 & percent_change  >= cutoff2 ~ "check",
+                                    change >= cutoff1 & percent_change < cutoff2 & geotype == 'cpa' ~ "check",
+                                    change < cutoff1 & percent_change >= cutoff2 ~ "check",
+                                    TRUE ~ "pass"))
+  return(df)
+}
+
+
+sort_dataframe <- function(df) {
+  df_fail <- unique(subset(df,pass.or.fail=="fail")$geozone)
+  df_check <- unique(subset(df,pass.or.fail=="check")$geozone)
+  df <- df %>% 
+    mutate(sort_order = case_when(geozone %in% df_fail ~ 1,
+                                  geozone %in% df_check ~ 2,
+                                  TRUE ~ 3))
+  df <- df[order(df$sort_order,df$geotype,df$geozone,df$yr_id),]
+  df$sort_order <- NULL
+  return(df)
+}
+
+rename_dataframe <- function(df) {
+  df <- df %>% rename('increment'= yr_id,'increment change' = change,
+                      'average annual change' = average_annual_change,
+                      'average annual percent change' = avg_ann_pct_chg,
+                      'increment percent change' = percent_change,
+                      'pass/fail' = pass.or.fail)
+  return(df)
+}
+
+
+add_id_for_excel_formatting <- function(df) {
+  t <- df %>% group_by(geozone) %>% tally()
+  if (nrow(subset(t,n!=9))!=0) {
+    print("ERROR: expecting 9 years per geography")
+    print(subset(t,n!=9)) } 
+  
+  ids <- rep(1:2, times=nrow(t)/2, each=9)
+  if (nrow(t)%%2!=0 ) {ids <- append(ids, c(1,1,1,1,1,1,1,1,1))}
+  df$id <- ids
+  return(df)
+}
+
+subset_by_geotype <- function(df,the_geotype) {
+  df1 <- subset(df,geotype == the_geotype)
+  df2 <- add_id_for_excel_formatting(df1)
+  df2 <- df2 %>% rename(!!the_geotype := geozone)
+  #df %>% rename(!!variable := name_of_col_from_df)
+  df2$geotype <- NULL
+  return(df2)
+}
---
+++ SR14Forecast/datasourceid29/Scripts/percent_change_spreadsheet.R
@@ -0,0 +1,181 @@
+
+maindir = dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(maindir)
+
+source("config.R")
+source("../Queries/readSQL.R")
+source("common_functions.R")
+source("functions_for_percent_change.R")
+
+packages <- c("RODBC","tidyverse","openxlsx")
+pkgTest(packages)
+
+# read email message from Dave and attach to excel spreadsheet
+fileName <- 'M:\\Technical Services\\QA Documents\\Projects\\Sub Regional Forecast\\6_Notes\\QATestsDavidTedrow.txt'
+email_msg <- read.delim(fileName)
+
+
+# connect to database
+channel <- odbcDriverConnect('driver={SQL Server}; server=sql2014a8; database=demographic_warehouse; trusted_connection=true')
+
+# get hhpop data
+hhvars <- readDB("../Queries/hh_hhp_hhs_ds_id.sql",datasource_id_current)
+jobs <- readDB("../Queries/jobs.sql",datasource_id_current)
+gq <- readDB("../Queries/group_quarter_w_description.sql",datasource_id_current)
+
+odbcClose(channel)
+
+merge1 <- merge(x = hhvars, y = jobs,by = c("datasource_id","yr_id","geotype","geozone"), all = TRUE)
+countvars <- merge(x = merge1, y = gq,by = c("datasource_id","yr_id","geotype","geozone"), all = TRUE)
+
+countvars <- subset(countvars, yr_id==2012 | yr_id==2016 | yr_id==2018 | yr_id==2020 | yr_id==2025 | yr_id==2030 | yr_id==2035 | yr_id==2040 | yr_id==2045 | yr_id==2050)
+
+countvars$geozone[countvars$geotype=='region'] <- 'San Diego Region'
+
+
+rm(merge1,hhvars,jobs,gq)
+
+
+subset(countvars,geozone=='San Diego Region')
+
+
+# clean up cpa names removing asterick and dashes etc.
+countvars <- rm_special_chr(countvars)
+countvars <- subset(countvars,geozone != 'Not in a CPA')
+#head(countvars)
+
+# order dataframe for doing lag calculation
+countvars <- countvars[order(countvars$datasource_id,countvars$geotype,countvars$geozone,countvars$yr_id),]
+
+jobs <- calculate_pct_chg(countvars, jobs)
+jobs <- calculate_pass_fail(jobs,5000,.20)
+jobs <- sort_dataframe(jobs)
+jobs <- rename_dataframe(jobs)
+jobs_cpa <- subset_by_geotype(jobs,'cpa')
+jobs_jur <- subset_by_geotype(jobs,'jurisdiction')
+
+households <- calculate_pct_chg(countvars, households)
+households <- calculate_pass_fail(households,2500,.20)
+households <- sort_dataframe(households)
+households <- rename_dataframe(households)
+households_cpa <- subset_by_geotype(households,'cpa')
+households_jur <- subset_by_geotype(households,'jurisdiction')
+
+hhp <- calculate_pct_chg(countvars, hhp)
+hhp <- calculate_pass_fail(hhp,7500,.20)
+hhp <- sort_dataframe(hhp)
+hhp <- rename_dataframe(hhp)
+hhp <- hhp %>% rename('hhpop'= hhp)
+hhp_cpa <- subset_by_geotype(hhp,'cpa')
+hhp_jur <- subset_by_geotype(hhp,'jurisdiction')
+
+units <- calculate_pct_chg(countvars, units)
+units <- calculate_pass_fail(units,2500,.20)
+units <- sort_dataframe(units)
+units <- rename_dataframe(units)
+units_cpa <- subset_by_geotype(units,'cpa')
+units_jur <- subset_by_geotype(units,'jurisdiction')
+
+gqpop <- calculate_pct_chg(countvars, gqpop)
+gqpop <- calculate_pass_fail(gqpop,500,.20)
+gqpop <- sort_dataframe(gqpop)
+gqpop <- rename_dataframe(gqpop)
+gqpop_cpa <- subset_by_geotype(gqpop,'cpa')
+gqpop_jur <- subset_by_geotype(gqpop,'jurisdiction')
+
+########################################################### 
+# create excel workbook
+
+wb = createWorkbook()
+
+# add sheet with email info
+sht1 = addWorksheet(wb, "Email")
+writeData(wb, sht1, email_msg)
+
+# add sheets with data 
+shtjurunits = addWorksheet(wb, "UnitsByJur", tabColour = "red")
+shtcpaunits = addWorksheet(wb, "UnitsByCPA", tabColour = "red")
+shtjurhh = addWorksheet(wb, "HHByJur", tabColour = "green")
+shtcpahh = addWorksheet(wb, "HHByCPA", tabColour = "green")
+shtjurhhp = addWorksheet(wb, "HHPopByJur", tabColour = "blue")
+shtcpahhp = addWorksheet(wb, "HHPopByCPA", tabColour = "blue")
+shtjurjobs = addWorksheet(wb, "JobsByJur", tabColour = "yellow")
+shtcpajobs = addWorksheet(wb, "JobsByCPA", tabColour = "yellow")
+shtjurgqpop = addWorksheet(wb, "GQPopByJur", tabColour = "purple")
+shtcpagqpop = addWorksheet(wb, "GQPopByCPA", tabColour = "purple")
+
+# formatting style
+negStyle <- createStyle(fontColour = "#9C0006", bgFill = "#FFC7CE")
+posStyle <- createStyle(fontColour = "#006100", bgFill = "#C6EFCE")
+checkStyle <- createStyle(fontColour = "#9C5700", bgFill = "#FFEB9C")
+lightgreyStyle <- createStyle(bgFill = "#dce6f1") #"#fcfcfa" #dce6f1 "#f5f5e6"
+darkgreyStyle <- createStyle(bgFill = "#c5d9f1") # "#e3e3e1" #c5d9f1 #b8cce4
+headerStyle <- createStyle(fontSize = 13, fontColour = "#FFFFFF", halign = "center",
+                           fgFill = "#4F81BD", border="TopBottom", borderColour = "#4F81BD",
+                           wrapText = TRUE)
+invisibleStyle <- createStyle(fontColour = "#FFFFFF")
+insideBorders <- openxlsx::createStyle(
+  border = c("top", "bottom", "left", "right"),
+  borderStyle = "dashed",borderColour="white"
+)
+rangeRowscpa = 2:(nrow(jobs_cpa)+1)
+rangeRowsjur = 2:(nrow(jobs_jur)+1)
+rangeCols = 1:9
+pct = createStyle(numFmt="0%") # percent 
+
+for (curr_sheet in names(wb)[-1]) {
+  addStyle(
+    wb = wb,
+    sheet = curr_sheet,
+    style = insideBorders,
+    rows = rangeRowscpa,
+    cols = rangeCols,
+    gridExpand = TRUE,
+    stack = TRUE
+  )
+  addStyle(wb, curr_sheet, style=pct, cols=c(6), rows=2:(nrow(hhp_cpa)+1), gridExpand=TRUE,stack = TRUE)
+  addStyle(wb, curr_sheet, style=pct, cols=c(8), rows=2:(nrow(hhp_cpa)+1), gridExpand=TRUE,stack = TRUE)
+  addStyle(wb, curr_sheet, headerStyle, rows = 1, cols = rangeCols, gridExpand = TRUE,stack = TRUE)
+  addStyle(wb, curr_sheet, style=invisibleStyle, cols=c(10), rows=1:(nrow(hhp_cpa)+1), gridExpand=TRUE,stack = TRUE)
+  setColWidths(wb, curr_sheet, cols = c(1,2,3,4,5,6,7,8,9), widths = c(16,14,33,15,16,18,18,18,14))
+  conditionalFormatting(wb, curr_sheet, cols=1:9, rows=1:(nrow(hhp_cpa)+1), rule="$J1==2", style = lightgreyStyle)
+  conditionalFormatting(wb, curr_sheet, cols=1:9, rows=1:(nrow(hhp_cpa)+1), rule="$J1==1", style = darkgreyStyle)
+  conditionalFormatting(wb, curr_sheet, cols=1:9, rows=2:(nrow(hhp_cpa)+1), type="contains", rule="fail", style = negStyle)
+  conditionalFormatting(wb, curr_sheet, cols=1:9, rows=2:(nrow(hhp_cpa)+1), type="contains", rule="check", style = checkStyle)
+}
+
+writeData(wb, shtjurunits,units_jur)
+writeData(wb, shtcpaunits,units_cpa)
+writeData(wb, shtjurhh,households_jur )
+writeData(wb, shtcpahh,households_cpa )
+writeData(wb, shtjurhhp,hhp_jur)
+writeData(wb, shtcpahhp,hhp_cpa)
+writeData(wb, shtjurjobs,jobs_jur)
+writeData(wb, shtcpajobs,jobs_cpa)
+writeData(wb, shtjurgqpop,gqpop_jur)
+writeData(wb, shtcpagqpop,gqpop_cpa)
+
+# add comment with cutoffs to each sheet
+c1 <- createComment(comment = "> 2,500 and > 20%")
+writeComment(wb, shtjurunits, col = "I", row = 1, comment = c1)
+writeComment(wb, shtcpaunits, col = "I", row = 1, comment = c1)
+writeComment(wb, shtjurhh, col = "I", row = 1, comment = c1)
+writeComment(wb, shtcpahh, col = "I", row = 1, comment = c1)
+c2 <- createComment(comment = "> 7,500 and > 20%")
+writeComment(wb, shtjurhhp, col = "I", row = 1, comment = c2)
+writeComment(wb, shtcpahhp, col = "I", row = 1, comment = c2)
+c3 <- createComment(comment = "> 5,000 and > 20%")
+writeComment(wb, shtjurjobs, col = "I", row = 1, comment = c3)
+writeComment(wb, shtcpajobs, col = "I", row = 1, comment = c3)
+c4 <- createComment(comment = "> 500 and > 20%")
+writeComment(wb, shtjurgqpop, col = "I", row = 1, comment = c4)
+writeComment(wb, shtcpagqpop, col = "I", row = 1, comment = c4)
+
+# out folder for excel
+outfolder<-paste("..\\Output\\",sep='')
+ifelse(!dir.exists(file.path(maindir,outfolder)), dir.create(file.path(maindir,outfolder), showWarnings = TRUE, recursive=TRUE),0)
+setwd(file.path(maindir,outfolder))
+
+saveWorkbook(wb, "EDAM_Forecast_var_counts.xlsx",overwrite=TRUE)
+
+
