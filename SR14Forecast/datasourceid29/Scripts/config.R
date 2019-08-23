fd72e9ec292fea7f233798635670729e9f1f6d28
SANDAGNET\aku <aku@sandag.org>
2019-08-23 09:49:04 -0700

    update config to datasource id 30

--- SR14Forecast/datasourceid29/Scripts/config.R
+++ SR14Forecast/datasourceid29/Scripts/config.R
@@ -1,16 +1,15 @@
 
 #LH added datasource_id_current
 
-datasource_id_current <- 28
+datasource_id_current <- 30
 
 
-# datasource_ids <- c(17,28)
-# datasource_names <- c("Series 14 (ds 17)","Series 14 (ds 28)")
-# datasource_name_short <- c("Series 14","Series 14")
-# datasource_outfolder <- "vacancy"
-
-datasource_ids <- c(13,29)
-datasource_names <- c("Series 13 (ds 13)","Series 14 (ds 29)")
-datasource_name_short <- c("Series 13","Series 14")
+datasource_ids <- c(17,30)
+datasource_names <- c("Series 14 (ds 17)","Series 14 (ds 30)")
+datasource_name_short <- c("Series 14","Series 14")
 datasource_outfolder <- "vacancy"
-#
+
+# datasource_ids <- c(13,30)
+# datasource_names <- c("Series 13 (ds 13)","Series 14 (ds 30)")
+# datasource_name_short <- c("Series 13","Series 14")
+# datasource_outfolder <- "vacancy"
