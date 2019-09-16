5e3b1a68e487dbebd870c7c73584b98f2839d515
skl <skl@sandag.org>
2019-09-16 10:38:58 -0700

    Query jobs data in the forcast and make plots for each geography

--- EDD/EDD_housing_link_functionized.sql
+++ EDD/EDD_housing_link_functionized.sql
@@ -1,20 +1,19 @@
-SELECT p.parcel_id AS parcel_id, 
-		m.emp_id AS emp_id, SUM(m.emp1) AS emp1,SUM(m.emp2) AS emp2,SUM(m.emp3) AS emp3, SUM(m.payroll) as payroll, SUM(m.naics) AS naics, m.dba,
-		b.building_id AS building_id, SUM(b.non_residential_sqft) AS nonresSqft,SUM(b.residential_sqft) AS resSqft, 
-		d.name --, SUM(dbo.GetDataForID(m.naics)) as Data
-From urbansim.urbansim.parcel p
+SELECT p.parcel_id AS parcel_id
+		,m.emp_id AS emp_id, SUM(m.emp1) AS emp1,SUM(m.emp2) AS emp2,SUM(m.emp3) AS emp3, SUM(m.payroll) as payroll, SUM(m.naics) AS naics, m.dba
+		,b.bldgID AS building_id--, SUM(b.non_residential_sqft) AS nonresSqft,SUM(b.residential_sqft) AS resSqft
+		,d.name --, SUM(dbo.GetDataForID(m.naics)) as Data
+From spacecore.urbansim.parcels p
 
-INNER JOIN urbansim.urbansim.building b
-	ON b.parcel_id = p.parcel_id
+INNER JOIN spacecore.GIS.building_outlines_divided b
+	ON b.parcelID = p.parcel_id
 
-INNER JOIN urbansim.ref.development_type as d
-	ON b.development_type_id = d.development_type_id
+INNER JOIN spacecore.ref.development_type as d
+	ON p.development_type_id_2017 = d.development_type_id
 
 --join OPENQUERY(sql2014b8, 'SELECT * FROM  EMPCORE.dbo.CA_EDD_EMP2017') AS m
 --	ON m.shape.STIntersects(b.shape) = 1
 
-Right JOIN OPENQUERY(sql2014b8, 'SELECT * FROM  EMPCORE.dbo.CA_EDD_EMP2017') AS m
+RIGHT JOIN OPENQUERY(sql2014b8, 'SELECT * FROM  EMPCORE.dbo.CA_EDD_EMP2017') AS m
 	ON m.shape.STIntersects(b.shape) = 1
 
-Group By p.parcel_id, m.emp_id, m.dba, d.name, b.building_id
-
+Group By p.parcel_id, m.emp_id, m.dba, d.name, b.bldgID
