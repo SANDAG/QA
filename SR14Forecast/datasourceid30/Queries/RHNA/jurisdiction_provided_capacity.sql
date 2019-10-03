f711059c7b11f2432510d9daa1fa598e0235d27d
anneku <aku@sandag.org>
2019-10-03 14:47:57 -0700

    add capacity query without sched dev

--- SR14Forecast/datasourceid30/Queries/RHNA/jurisdiction_provided_capacity.sql
+++ SR14Forecast/datasourceid30/Queries/RHNA/jurisdiction_provided_capacity.sql
@@ -7,3 +7,13 @@ SELECT jurisdiction_id, sum(COALESCE(capacity_3,capacity_2)) as jur_provided_cap
     ON scheduled_development_parcel.parcel_id = parcel.parcel_id
     GROUP BY jurisdiction_id
 	ORDER by jurisdiction_id
+
+
+-- without scheduled development
+
+	SELECT jurisdiction_id, sum(capacity_2) as jur_provided_cap
+    FROM [urbansim].[urbansim].[parcel]
+    LEFT JOIN  [urbansim].[urbansim].[scheduled_development_parcel]
+    ON scheduled_development_parcel.parcel_id = parcel.parcel_id
+    GROUP BY jurisdiction_id
+	ORDER by jurisdiction_id
