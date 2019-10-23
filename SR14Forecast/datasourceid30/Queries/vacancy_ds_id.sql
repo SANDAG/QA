c5e241059dd85efcab64dddeb4c84772389e9bb3
anneku <aku@sandag.org>
2019-10-23 15:22:57 -0700

    add datasource id as variable to vacancy script

--- SR14Forecast/datasourceid30/Queries/vacancy_ds_id.sql
+++ SR14Forecast/datasourceid30/Queries/vacancy_ds_id.sql
@@ -1,3 +1,7 @@
+-- Forecast of unoccupiable units for region
+
+DECLARE @ds_id int
+SET @ds_id = 30
 
 USE demographic_warehouse;
 
@@ -13,19 +17,26 @@ SELECT
 		  WHEN (SUM(housing.units))=0 THEN 0 
 		  ELSE round(1 - SUM(housing.occupied)/CAST(SUM(housing.units) as float),4) 
 	END as vacancy_rate
+	,CASE
+		  WHEN (SUM(housing.units))=0 THEN 0 
+		  ELSE round(1 - SUM(housing.occupied)/CAST((SUM(housing.units) - 
+		  SUM(housing.unoccupiable))as float),4) 
+	END as vacancy_rate_effective
 FROM fact.housing
 	INNER JOIN dim.mgra
 	ON mgra.mgra_id = housing.mgra_id
+	--AND mgra.geotype IN ('region')
 	AND mgra.geotype IN ('jurisdiction', 'region', 'cpa')
-WHERE housing.datasource_id IN (ds_id) AND housing.yr_id NOT IN (2023,2026,2029,2032)
+--WHERE housing.datasource_id IN (ds_id) AND housing.yr_id NOT IN (2023,2026,2029,2032)
+WHERE housing.datasource_id IN (ds_id) 
 GROUP BY 
 	housing.yr_id
 	,mgra.geotype
 	,mgra.geozone
 	,housing.datasource_id
-order by geotype,geozone,yr_id,datasource_id
+ORDER BY geotype,geozone,yr_id,datasource_id
 
 -- total rows
---864 rows
---SELECT (87 + 1 + 1 + 19) * 8	
---note 87 cpas + 1 not in a cpa + 1 region + 19 (18 jurisdictions + unincorporated) x 8 increments
+--972 rows
+--SELECT (87 + 1 + 1 + 19) * 9	
+--note 87 cpas + 1 not in a cpa + 1 region + 19 (18 jurisdictions + unincorporated) x 9 increments
