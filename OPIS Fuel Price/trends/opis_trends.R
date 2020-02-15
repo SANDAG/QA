c91d0522957d8d8915933faab77afb156dbd72fb
lisbethhow <lisbeth.howard@sandag.org>
2020-02-15 13:22:25 -0800

    change reference to data pulled, fixes for output - rename columns and revise plots

--- OPIS Fuel Price/trends/opis_trends.R
+++ OPIS Fuel Price/trends/opis_trends.R
@@ -1,6 +1,5 @@
 #look up opis data online for data dictionary.
-##NEED TO CHANGE DATE FORMAT FOR EIA DATA? SEE ROW 93
-#need to add california sheets or column - waiting for Cherry
+
 
 #function to load packages
 pkgTest <- function(pkg){
@@ -13,9 +12,12 @@ pkgTest <- function(pkg){
 
 #identify packages to be loaded
 packages <- c("sqldf", "rstudioapi", "RODBC", "dplyr", "reshape2", 
-              "stringr","tidyverse", "readxl", "frequency", "ggplot", "lubridate")
+              "stringr","tidyverse", "readxl", "frequency", "ggplot2", "lubridate")
 #confirm packages are read in
 pkgTest(packages)
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+source("../Queries/readSQL.R")
 
 options(stringsAsFactors=FALSE)
 
@@ -32,7 +34,7 @@ on a.date_id=b.date_id"
 
 
 #read in EIA data
-#need to add california sheets or column - waiting for Cherry
+#us - all areas all formulations
 
 eia_reg_us <- read_excel("M:\\Technical Services\\QA Documents\\Projects\\OPIS and CARB Fleet\\Data Files\\EIA gas price data\\pswrgvwall (1).xlsx", sheet = "Data 3", skip = 2)[,1:2]
 
@@ -43,128 +45,193 @@ eia_prem_us <- read_excel("M:\\Technical Services\\QA Documents\\Projects\\OPIS 
 eia_diesel_us <- read_excel("M:\\Technical Services\\QA Documents\\Projects\\OPIS and CARB Fleet\\Data Files\\EIA gas price data\\psw18vwall.xlsx", sheet = "Data 2", skip = 2)[,1:2]
 
 
-#set the columns to select
-# eia_reg_ca <- read_excel("M:\\Technical Services\\QA Documents\\Projects\\OPIS and CARB Fleet\\Data Files\\EIA gas price data\\pswrgvwall (1).xlsx", sheet = "Data 3", skip = 2)[,1:2]
-# 
-# eia_mid_ca <- read_excel("M:\\Technical Services\\QA Documents\\Projects\\OPIS and CARB Fleet\\Data Files\\EIA gas price data\\pswrgvwall (1).xlsx", sheet = "Data 6", skip = 2)[,1:2]
-# 
-# eia_prem_ca <- read_excel("M:\\Technical Services\\QA Documents\\Projects\\OPIS and CARB Fleet\\Data Files\\EIA gas price data\\pswrgvwall (1).xlsx", sheet = "Data 9", skip = 2)[,1:2]
-# 
-# eia_diesel_ca <- read_excel("M:\\Technical Services\\QA Documents\\Projects\\OPIS and CARB Fleet\\Data Files\\EIA gas price data\\psw18vwall.xlsx", sheet = "Data 2", skip = 2)[,1:2]
-
-
+#california - reformulated only
+eia_reg_ca <- read_excel("M:\\Technical Services\\QA Documents\\Projects\\OPIS and CARB Fleet\\Data Files\\EIA gas price data\\pswrgvwall (1).xlsx", sheet = "Data 2", skip = 2)[,c(1,10)]
+ 
+eia_mid_ca <- read_excel("M:\\Technical Services\\QA Documents\\Projects\\OPIS and CARB Fleet\\Data Files\\EIA gas price data\\pswrgvwall (1).xlsx", sheet = "Data 5", skip = 2)[,c(1,10)]
+ 
+eia_prem_ca <- read_excel("M:\\Technical Services\\QA Documents\\Projects\\OPIS and CARB Fleet\\Data Files\\EIA gas price data\\pswrgvwall (1).xlsx", sheet = "Data 8", skip = 2)[,c(1,10)]
+ 
+eia_diesel_ca <- read_excel("M:\\Technical Services\\QA Documents\\Projects\\OPIS and CARB Fleet\\Data Files\\EIA gas price data\\psw18vwall.xlsx", sheet = "Data 2", skip = 2)[,c(1,11)]
 
 head(eia_reg_us)
 head(eia_mid_us)
 head(eia_prem_us)
 head(eia_diesel_us)
+head(eia_reg_ca)
+head(eia_mid_ca)
+head(eia_prem_ca)
+head(eia_diesel_ca)
 
+#add column to indicate fuel grade
+eia_reg_us$product <- "Unleaded Gas"
+eia_mid_us$product <- "Midgrade Gas"
+eia_prem_us$product <- "Premium Gas"
+eia_diesel_us$product <- "Diesel"
 
-#rename eia data columns
-names(eia_reg_us)[2] <- "eia_reg_ret_avg"
-names(eia_mid_us)[2] <- "eia_mid_ret_avg"
-names(eia_prem_us)[2] <- "eia_prem_ret_avg"
-names(eia_diesel_us)[2] <- "eia_diesel_ret_avg"
+eia_reg_ca$product <- "Unleaded Gas"
+eia_mid_ca$product <- "Midgrade Gas"
+eia_prem_ca$product <- "Premium Gas"
+eia_diesel_ca$product <- "Diesel"
 
-#run frequencies
-#options(frequency_render = FALSE)
-freq(opis[, c('region','product','yr','season')])
+#add column to indicate data source
+eia_reg_us$source <- "US_EIA"
+eia_mid_us$source <- "US_EIA"
+eia_prem_us$source <- "US_EIA"
+eia_diesel_us$source <- "US_EIA"
 
-#options(frequency_open_output = TRUE)
+eia_reg_ca$source <- "State of CA-EIA"
+eia_mid_ca$source <- "State of CA-EIA"
+eia_prem_ca$source <- "State of CA-EIA"
+eia_diesel_ca$source <- "State of CA-EIA"
 
-opis$date_code_rc <- as.Date(opis$date_code, format = "%Y-%m-%d")
+#rename eia columns with price
+names(eia_reg_us)[2] <- "retail_avg"
+names(eia_mid_us)[2] <- "retail_avg"
+names(eia_prem_us)[2] <- "retail_avg"
+names(eia_diesel_us)[2] <- "retail_avg"
 
-#opis$date_code_rc <- as.Date(opis$date_code_rc1, format = '%b-%y')
-
-#confirm opis$data_code_rc is date format
-IsDate <- function(mydate) {
-  tryCatch(!is.na(as.Date(opis$date_code_rc, "",tryFormats = c("%Y-%m-%d", "%Y/%m/%d","%d-%m-%Y","%m-%d-%Y"))),  
-           error = function(err) {FALSE})  
-}
-
-IsDate(opis$date_code_rc)
-IsDate(eia_reg_us$Date)
+names(eia_reg_ca)[2] <- "retail_avg"
+names(eia_mid_ca)[2] <- "retail_avg"
+names(eia_prem_ca)[2] <- "retail_avg"
+names(eia_diesel_ca)[2] <- "retail_avg"
 
 #recode EIA date
 eia_reg_us$date <- as.Date(eia_reg_us$Date, format = "%Y-%m-%d")
+eia_mid_us$date <- as.Date(eia_mid_us$Date, format = "%Y-%m-%d")
+eia_prem_us$date <- as.Date(eia_prem_us$Date, format = "%Y-%m-%d")
+eia_diesel_us$date <- as.Date(eia_diesel_us$Date, format = "%Y-%m-%d")
+
+eia_reg_ca$date <- as.Date(eia_reg_ca$Date, format = "%Y-%m-%d")
+eia_mid_ca$date <- as.Date(eia_mid_ca$Date, format = "%Y-%m-%d")
+eia_prem_ca$date <- as.Date(eia_prem_ca$Date, format = "%Y-%m-%d")
+eia_diesel_ca$date <- as.Date(eia_diesel_ca$Date, format = "%Y-%m-%d")
+
+#exclude data before 2005 to make data comparable to opis 
 eia_reg_us <- eia_reg_us[eia_reg_us$date>="2005-01-01",]
+eia_mid_us <- eia_mid_us[eia_mid_us$date>="2005-01-01",]
+eia_prem_us <- eia_prem_us[eia_prem_us$date>="2005-01-01",]
+eia_diesel_us <- eia_diesel_us[eia_diesel_us$date>="2005-01-01",]
 
-head(eia_reg_us$date)
+eia_reg_ca <- eia_reg_ca[eia_reg_ca$date>="2005-01-01",]
+eia_mid_ca <- eia_mid_ca[eia_mid_ca$date>="2005-01-01",]
+eia_prem_ca <- eia_prem_ca[eia_prem_ca$date>="2005-01-01",]
+eia_diesel_ca <- eia_diesel_ca[eia_diesel_ca$date>="2005-01-01",]
 
+#drop original date variable
+eia_reg_us$Date <- NULL
+eia_mid_us$Date <- NULL
+eia_prem_us$Date <- NULL
+eia_diesel_us$Date <- NULL
+eia_reg_ca$Date <- NULL
+eia_mid_ca$Date <- NULL
+eia_prem_ca$Date <- NULL
+eia_diesel_ca$Date <- NULL
 
+head(eia_reg_us)
+head(eia_mid_us)
+head(eia_prem_us)
+head(eia_diesel_us)
+head(eia_reg_ca)
+head(eia_mid_ca)
+head(eia_prem_ca)
+head(eia_diesel_ca)
 
-opis_reg_us <- opis[(opis$product=="Unleaded Gas" | opis$product=="Regular Gas"),]
-#calculate average price across gas types
-#opis_agg <- opis  %>%  group_by(date_code_rc) %>% tally(retail_avg)
-#opis_agg$retail_avg_rc <- opis_agg$n/4
+eia_all <- rbind(eia_reg_us,eia_mid_us,eia_prem_us,eia_diesel_us,eia_reg_ca,eia_mid_ca,eia_prem_ca,eia_diesel_ca)
+
+head(eia_all)
+rm(eia_reg_us,eia_mid_us,eia_prem_us,eia_diesel_us,eia_reg_ca,eia_mid_ca,eia_prem_ca,eia_diesel_ca)
+
+#run frequencies
+#options(frequency_render = FALSE)
+#options(frequency_open_output = TRUE)
+colnames(opis)
+
+freq(opis[, c('region','product','yr','season')])
+opis$date <- as.Date(opis$date_code, format = "%Y-%m-%d")
+opis$source <- "San Diego_OPIS"
+opis <- opis %>% select(retail_avg,product,source,date)
+
+head(opis)
+head(eia_all)
+
+table(opis$product)
+table(eia_all$product)
+opis_eia <- rbind(opis,eia_all)
+opis_eia <- opis_eia[opis_eia$date<"2019-06-01",]
+
+class(opis_eia$retail_avg)
+class
+opis_eia_reg <- opis_eia[(opis_eia$product=="Unleaded Gas"),]
+opis_eia_mid <- opis_eia[(opis_eia$product=="Midgrade Gas"),]
+opis_eia_prem <- opis_eia[(opis_eia$product=="Premium Gas"),]
+opis_eia_diesel <- opis_eia[(opis_eia$product=="Diesel"),]
 
 
 maindir = dirname(rstudioapi::getSourceEditorContext()$path)
 results <- "plots\\opis_fuel\\"
 ifelse(!dir.exists(file.path(maindir,results)), dir.create(file.path(maindir,results), showWarnings = TRUE, recursive=TRUE),0)
 
-head(opis)
-head(eia_reg_us)
-
-p <- ggplot() + geom_line(data=opis_reg, aes(x=date_code_rc, y = retail_avg), color= "red") + 
-#  geom_line(data=eia_reg_ca, aes(x=date,y=eia_reg_ret_avg), color = "green")
-  geom_line(data=eia_reg_us, aes(x=date,y=eia_reg_ret_avg), color = "blue")
-  scale_y_continuous(limits = c(2, 3.5)) +
+plot_reg <- ggplot() + geom_line(data=opis_eia_reg, aes(x=date, y = retail_avg, group=source, color=source)) + 
+  scale_y_continuous(limits = c(2, 4.5)) +
   theme_bw(base_size = 14) +  theme(plot.title = element_text(hjust = 0.5)) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(axis.text.x = element_text(hjust = 0.5)) +
   theme(legend.position = "bottom",
         legend.title=element_blank(),
         plot.caption = element_text(size = 9)) +
-  labs(title="OPIS and EIA Gas Price Comparison:\n2005 through September 2019",
+  labs(title="OPIS and EIA Regular Unleaded Gas Price Comparison:\n2005 through May 2019",
        y="Retail Average Price per Gallon", x="Year",
-       caption="Sources: OPIS, EIA; Notes: US retail average is based on [TBD- add info about formulation]; Due to differences in the availability of the data, OPIS data are shown by month whereas EIA data are available by week.")
-results<-"plots\\opis\\"
-ggsave(output, file= paste(results, 'opis_reg.png'))
-       #width=6, height=8, dpi=100)#, scale=2)
+       caption="Sources: OPIS, EIA; Notes: US retail average is based on all areas and all formulations and CA is reformulated only;\nDue to differences in the availability of the data, OPIS data are shown by month whereas EIA data are available by week.")
+  ggsave(plot_reg, file=paste(results,"Regular Unleaded OPIS to EIA Comparison.png", sep = ''),
+  width=10, height=6, dpi=100)#, scale=2)
+  
+plot_reg
+ 
+getwd() 
+plot_mid <- ggplot() + geom_line(data=opis_eia_mid, aes(x=date, y = retail_avg, group=source, color=source)) + 
+  scale_y_continuous(limits = c(2, 4.5)) +
+  theme_bw(base_size = 14) +  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(axis.text.x = element_text(hjust = 1)) +
+  theme(legend.position = "bottom",
+        legend.title=element_blank(),
+        plot.caption = element_text(size = 9)) +
+  labs(title="OPIS and EIA Midgrage Unleaded Gas Price Comparison:\n2005 May 2019",
+       y="Retail Average Price per Gallon", x="Year",
+       caption="Sources: OPIS, EIA; Notes: US retail average is based on all areas and all formulations and CA is reformulated only;\nDue to differences in the availability of the data, OPIS data are shown by month whereas EIA data are available by week.")
+  ggsave(plot_reg, file= paste(results, "Midgrade Unleaded OPIS to EIA Comparison"),
+         width=10, height=6, dpi=100)#, scale=2)
 
+plot_mid
 
-p
+plot_prem <- ggplot() + geom_line(data=opis_eia_prem, aes(x=date, y = retail_avg, group=source, color=source)) + 
+  scale_y_continuous(limits = c(2, 4.5)) +
+  theme_bw(base_size = 14) +  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(axis.text.x = element_text(hjust = 1)) +
+  theme(legend.position = "bottom",
+        legend.title=element_blank(),
+        plot.caption = element_text(size = 9)) +
+  labs(title="OPIS and EIA Premium Unleaded Gas Price Comparison:\n2005 through May 2019",
+       y="Retail Average Price per Gallon", x="Year",
+       caption="Sources: OPIS, EIA; Notes: US retail average is based on all areas and all formulations and CA is reformulated only;\nDue to differences in the availability of the data, OPIS data are shown by month whereas EIA data are available by week.")
+ggsave(plot_reg, file= paste(results, "Premium Unleaded OPIS to EIA Comparison"),
+       width=10, height=6, dpi=100)#, scale=2)
 
+plot_prem  
 
+plot_diesel <- ggplot() + geom_line(data=opis_eia_mid, aes(x=date, y = retail_avg, group=source, color=source)) + 
+  scale_y_continuous(limits = c(2, 4.5)) +
+  theme_bw(base_size = 14) +  theme(plot.title = element_text(hjust = 0.5)) +
+  theme(axis.text.x = element_text(hjust = 1)) +
+  theme(legend.position = "bottom",
+        legend.title=element_blank(),
+        plot.caption = element_text(size = 9)) +
+  labs(title="OPIS and EIA Midgrage Unleaded Gas Price Comparison:\n2005 through May 2019",
+       y="Retail Average Price per Gallon", x="Year",
+       caption="Sources: OPIS, EIA; Notes: US retail average is based on all areas and all Diesel types and CA is reformulated only;\nDue to differences in the availability of the data, OPIS data are shown by month whereas EIA data are available by week.")
+ggsave(plot_reg, file= paste(results, "Diesel All Types OPIS to EIA Comparison"),
+       width=10, height=6, dpi=100)#, scale=2)
 
-############################
-############################
-
-#for(i in jur_list) {
- # plotdat = subset(hh_jur, hh_jur$cityname==i)
-  plot<-ggplot(opis_agg, aes(date_code_rc)) + 
-    geom_line(aes(y = us_price, colour = "1_Region",group=0),size=1.5) +
-    geom_point(size=3,aes(y=reg_hhs,color="1_Region")) +
-    geom_line(aes(y = hhs, colour = cityname,group=0),size=1.5) + 
-    geom_point(size=3,aes(y=hhs,colour=cityname)) +
-    scale_y_continuous(limits = c(2, 3.5)) +
-    theme_bw(base_size = 14) +  theme(plot.title = element_text(hjust = 0.5)) +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-    theme(legend.position = "bottom",
-          legend.title=element_blank(),
-          plot.caption = element_text(size = 9)) +
-    labs(title=paste("Household Size:", i,' and Region\n datasource_id ',ds_id,sep=''), 
-         y="Household size", x="Year",
-         caption=paste("Sources: demographic_warehouse",ds_id,"; Notes: Refer to table below for out of range hh size values",sep=''))
-  results<-"plots\\hhsize\\jur\\"
-  output_table<-plotdat[,c("yr_id","hhp","households","hhs","reg_hhp","reg_hh","reg_hhs")]
-  colnames(output_table)[colnames(output_table)=="households"] <- "hh"
-  tt <- ttheme_default(base_size=12,colhead=list(fg_params = list(parse=TRUE)))
-  tbl <- tableGrob(output_table, rows=NULL, theme=tt)
-  lay <- rbind(c(1,1,1,1,1),
-               c(1,1,1,1,1),
-               c(1,1,1,1,1),
-               c(2,2,2,2,2),
-               c(2,2,2,2,2))
-  output<-grid.arrange(plot,tbl,ncol=1,as.table=TRUE,layout_matrix=lay)
-  ggsave(output, file= paste(results, 'hhsize', i, ds_id,".png", sep=''),
-         width=6, height=8, dpi=100)#, scale=2)
-}
-
-
-
-
-
+plot_diesel
 
 
 
