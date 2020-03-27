698b762e14218f3d6dabdd4db588178b7994a324
SANDAGNET\kte <kte@sandag.org>
2020-03-26 19:59:56 -0700

    update ds_id to 33 to retrieve estimates

---
+++ Estimates/estimates ds_id 33/Queries/median_age_cpa.sql
@@ -0,0 +1,4 @@
+use demographic_warehouse
+
+EXECUTE dbo.compute_median_age_all_zones 33, 'cpa'
+ 
---
+++ Estimates/estimates ds_id 33/Queries/median_age_reg.sql
@@ -0,0 +1,3 @@
+use demographic_warehouse
+
+EXECUTE dbo.compute_median_age_all_zones 33, 'region'
