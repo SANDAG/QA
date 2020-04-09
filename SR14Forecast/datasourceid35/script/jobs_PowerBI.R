1a6d3534e24b4cd9ca0bc85d52458bd34dd4e9b5
lisbethhow <lisbeth.howard@sandag.org>
2020-04-09 09:26:20 -0700

    add region data and save to Sharepoint command

--- SR14Forecast/datasourceid35/script/jobs_PowerBI.R
+++ SR14Forecast/datasourceid35/script/jobs_PowerBI.R
@@ -1,5 +1,5 @@
 #job by industry
-##add save out location
+##revise save out location as needed
 
 
 datasource_id=35
@@ -33,10 +33,20 @@ jobs_jur_sql = getSQL("../Queries/jobs_jur_PowerBI.sql")
 jobs_jur_sql <- gsub("ds_id", datasource_id, jobs_jur_sql)
 jobs_jur<-sqlQuery(channel,jobs_jur_sql)
 
+jobs_reg_sql = getSQL("../Queries/jobs_region_PowerBI.sql")
+jobs_reg_sql <- gsub("ds_id", datasource_id, jobs_reg_sql)
+jobs_reg<-sqlQuery(channel,jobs_reg_sql)
 
 odbcClose(channel)
 
 #save out files
-write.csv(jobs_jur, paste("",".csv",sep=""))
 
-write.csv(jobs_cpa, paste("",,".csv",sep=""))
+https://sandag.sharepoint.com/:f:/r/qaqc/Shared%20Documents/Forecast/Results/ID%2035?csf=1&web=1&e=Irmd0Z
+
+write.csv(jobs_cpa, paste("C:\\Users\\lho\\San Diego Association of Governments\\SANDAG QA QC - Documents\\Forecast\\Results\\ID ",datasource_id,"\\Results\\jobs_cpa.csv",sep=""))
+
+write.csv(jobs_jur, paste("C:\\Users\\lho\\San Diego Association of Governments\\SANDAG QA QC - Documents\\Forecast\\Results\\ID ",datasource_id,"\\Results\\jobs_jur.csv",sep=""))
+
+write.csv(jobs_reg, paste("C:\\Users\\lho\\San Diego Association of Governments\\SANDAG QA QC - Documents\\Forecast\\Results\\ID ",datasource_id,"\\Results\\jobs_region.csv",sep=""))
+
+"C:\\Users\\psi\\San Diego Association of Governments\\SANDAG QA QC - Documents\\Estimates\\ds_id=33\\Results\\file_name.csv"
