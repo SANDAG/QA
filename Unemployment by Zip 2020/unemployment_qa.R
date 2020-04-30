622fe4116be30370d1a1e941e3b24f10127a7889
KelsieTelson-SANDAG <46978506+KelsieTelson-SANDAG@users.noreply.github.com>
2020-04-30 08:39:02 -0700

    Update unemployment_qa.R

--- Unemployment by Zip 2020/unemployment_qa.R
+++ Unemployment by Zip 2020/unemployment_qa.R
@@ -1,6 +1,12 @@
 #Purpose: Script to be used on a re-occuring basis to QA/QC unemployment data weekly. 
+#Test 1: Confirm  ZIP codes in datafile match master list (109 total) 
+#Test 2: Confirm LBF column equals total as provided by Stephanie (defined as "x" in script)
+#Test 3: Confirm percentages in most recent date column (last column in dataset) are calculated correctly based on preceeding column
+# containing whole number divided by LBF value in same row. 
 #Authors: Kelsie Telson and Purva Singh
 
+#total for LBF (manually update as needed based on Stephanie)
+x<- 1734783
 
 #load packages
 pkgTest <- function(pkg){
@@ -12,13 +18,13 @@ pkgTest <- function(pkg){
   
 }
 packages <- c("data.table", "ggplot2", "scales", "sqldf", "rstudioapi", "RODBC", "plyr", "dplyr", "reshape2", 
-              "stringr","gridExtra","grid","lattice","gtable")
+              "stringr","gridExtra","grid","lattice","gtable","readxl", "openxlsx", "tidyverse")
 pkgTest(packages)
 
 #set working directory to access files and save to
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-#open connection to retreive data
+#open connection to retreive data from database
 channel <- odbcDriverConnect('driver={SQL Server}; server=sql2014a8; database=demographic_warehouse; trusted_connection=true')
 raw_dt<-data.table::as.data.table(
   RODBC::sqlQuery(channel,
@@ -27,46 +33,74 @@ raw_dt<-data.table::as.data.table(
                   stringsAsFactors = FALSE),
   stringsAsFactors = FALSE)
 
-#define x as most recent date variable
+#rename columns of interest into friendly names (always last two columns in data pull because those are the most recent)
+rename <- function(x, y, z) {
+  x %>% 
+    rename_at(
+      vars(
+        names(.) %>%
+          tail(y)),
+      funs(paste(z))
+    )
+}
+
+raw_dt<- raw_dt %>% rename(., y = 2, z = c("number", "percent"))
 
 
+#retrieve master zip code list from Sharepoint site
+zip<- read_excel("C:\\Users\\kte\\San Diego Association of Governments\\SANDAG QA QC - Documents\\Weekly Employment Report\\Data\\ags ZIP codes.xlsx", 
+                   sheet = "Sheet1",
+                   range= "A1:A110")
 
+##Test 1
+#sort both files by Zip code
+raw_dt<-raw_dt[order(raw_dt$ZI),]
+zip<-zip[order(zip$ZI),]
+#create object with Zip code and flag for TRUE/FALSE
+test1<- as.data.table(list(raw_dt$ZI, raw_dt$ZI==zip$ZI))
 
+##Test 2
+test2<-as.data.table(sum(raw_dt$LBF)==x)
 
-
-
-
-
-
-
+##Test 3
+#recreate percentage for QA comparison
+raw_dt$qa_perc <- raw_dt$number/raw_dt$LBF
+raw_dt$qa_perc<- (raw_dt$qa_perc*100)
+#create column with flag for TRUE/FALSE
+raw_dt$test3<- as.data.table(round(raw_dt$percent, digits=2)==round(raw_dt$qa_perc, digits=2))
+#create object with variables of interest
+test3<- subset(raw_dt, 
+               select=c("ZI","LBF", "number", "percent", "qa_perc", "test3"))
 
 
 #############################################################
 #Saving data 
 wb= createWorkbook()
 
-inp <- addWorksheet(wb, "Input_Raw",tabColour="purple")
-writeData(wb,inp, input)
+test_1 <- addWorksheet(wb, "Test 1",tabColour="purple")
+writeData(wb,test_1, test1)
 
-outchk <- addWorksheet(wb, "QA_Test",tabColour="red")
-writeData(wb, outchk,output_check)
+test_2 <- addWorksheet(wb, "Test 2",tabColour="red")
+writeData(wb,test_2, test2)
+
+test_3 <- addWorksheet(wb, "Test 3",tabColour="green")
+writeData(wb,test_3, test3)
 
 
 #saving excel in output folder of the working directory
-setwd("C:\\Users\\kte\\San Diego Association of Governments\\SANDAG QA QC - Documents\\Weekly Employment Report\\Results")
+maindir<-setwd("C:\\Users\\kte\\San Diego Association of Governments\\SANDAG QA QC - Documents\\Weekly Employment Report")
 
 # excel workbook output file name and folder with timestamp
-now <- Sys.time()
-#outputfile <- paste("Ethnicity","_ds",datasource_id_current,"_",format(now, "%Y%m%d"),".xlsx",sep='')
-outputfile <- paste("Employment_","Weekly_","QA","DATE",".xlsx",sep='')
+now <- format(Sys.time(), "%Y%m%d")
+outputfile <- paste("Weekly_","Unemployment","_QA_",now,".xlsx",sep='')
 print(paste("output filename: ",outputfile))
 
 
-outfile <- paste(maindir,"/",outfolder,outputfile,sep='')
-
-outfolder<-paste("..\\Results\\",sep='')
+outfolder<-paste("Results/",sep='')
 ifelse(!dir.exists(file.path(maindir,outfolder)), dir.create(file.path(maindir,outfolder), showWarnings = TRUE, recursive=TRUE),0)
-setwd(file.path(maindir,outfolder))
 
-getwd()
+outfile <- paste(maindir,"/",outfolder,outputfile,sep='')
+print(paste("output filepath: ",outfile))
+
 saveWorkbook(wb, outfile,overwrite=TRUE)
+
