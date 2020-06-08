d8b4bd44c173cfdc7045ff039c09db732dc33ebc
KelsieTelson-SANDAG <46978506+KelsieTelson-SANDAG@users.noreply.github.com>
2020-06-08 16:50:49 -0700

    Update household_units.R

--- 2020-05 SCS Regional Forecast/R/household_units.R
+++ 2020-05 SCS Regional Forecast/R/household_units.R
@@ -4,7 +4,6 @@
 # Tests included in this report are:
 #   
 # Test #3: Household Units (Performance Analysis)
-# Test #5: Scheduled Development (Information Item)
 # 
 # Thorough descriptions of these tests may be found here: 
 # https://sandag.sharepoint.com/qaqc/_layouts/15/Doc.aspx?sourcedoc={17b5d132-f152-49f3-8c62-0b2791a26dd4}&action=edit&wd=target%28Test%20Plan.one%7Cdb0a3336-7736-46dc-afd0-8707889af1a0%2FOverview%7C5e8c7ac7-61e5-40fa-8d9c-6ae544b57ed1%2F%29
@@ -77,7 +76,7 @@ datasource_id<- 36
 hh <- readDB("../queries/households.sql",datasource_id)
 
 #select only years of interest from SCS forecast dataset
-hh<-subset(hh, hh$yr_id==2016 | hh$yr_id==2050)
+hh<-subset(hh, hh$yr_id==2018 | hh$yr_id==2050)
 
 #convert to data table
 hh<-as.data.table(hh)
@@ -104,14 +103,14 @@ hh_agg<-hh[ , list(
   by="mgra"]
 
 #check subtotals
-hh_agg_16<-subset(hh_agg, hh_agg$yr_id==2016)
+hh_agg_18<-subset(hh_agg, hh_agg$yr_id==2018)
 hh_agg_50<-subset(hh_agg, hh_agg$yr_id==2050)
 sum(hh_agg_50$units)
 
 #create aggregate table by mgra for 2016 and 2050
 hh_agg<-as.data.table(hh_agg)
 hh_reshape<- hh_agg[, list(
-  units_2016=units[yr_id==2016],
+  units_2018=units[yr_id==2018],
   units_2050=units[yr_id==2050]),
   by="mgra"]
 
@@ -135,14 +134,15 @@ hh_inputs_agg<-aggregate(scenario_cap ~mgra+tier, data=input_hhunits, sum)
 #merge scs forecast data to inputs data
 hh_merged<-merge(hh_reshape2,
                  hh_inputs_agg,
-                 by= "mgra",
+                 by.x= "Group.1",
+                 by.y= "mgra",
                  all.x=TRUE)
 
 #check subtotals
 sum(hh_merged$units_2050)
 
 #calculate difference between 2050 and 2016 units
-hh_merged$N_diff<- hh_merged$units_2050-hh_merged$units_2016
+hh_merged$N_diff<- hh_merged$units_2050-hh_merged$units_2018
 
 #apply flag to indicate relationship between expected change and actual change in units
 hh_merged$change[hh_merged$N_diff==hh_merged$scenario_cap]<- "Exact capacity change"
@@ -152,7 +152,7 @@ hh_merged$change[hh_merged$N_diff==0]<- "No Change"
 
 
 #save out data file
-write.csv(hh_merged, "C://Users//kte//OneDrive - San Diego Association of Governments//QA temp//SCS//housing_units_mgra.csv")
+write.csv(hh_merged, "C://Users//kte//OneDrive - San Diego Association of Governments//QA temp//SCS//housing_units_mgra_2018base.csv")
 
 ########################################################################################################
 
@@ -172,7 +172,7 @@ hh_jur<- merge(hh_jur,
                by.y="cap_jurisdiction_id")
 
 #calculate difference between 2050 and 2016 units
-hh_jur$N_diff<- hh_jur$`2050`-hh_jur$`2016`
+hh_jur$N_diff<- hh_jur$`2050`-hh_jur$`2018`
 
 #apply flag to indicate relationship between expected change and actual change in units
 hh_jur$change[hh_jur$N_diff==hh_jur$scenario_cap]<- "Exact capacity change"
@@ -181,7 +181,7 @@ hh_jur$change[hh_jur$N_diff>hh_jur$scenario_cap]<- "Greater than capacity"
 hh_jur$change[hh_jur$N_diff==0]<- "No Change"
 
 #save out data file
-write.csv(hh_jur, "C://Users//kte//OneDrive - San Diego Association of Governments//QA temp//SCS//housing_units_jur.csv")
+write.csv(hh_jur, "C://Users//kte//OneDrive - San Diego Association of Governments//QA temp//SCS//housing_units_jur_2018base.csv")
 
 
 
