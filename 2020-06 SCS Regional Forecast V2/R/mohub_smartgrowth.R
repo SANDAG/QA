cc8002018ff831989832f7103c1fdebcf24c0b6e
KelsieTelson-SANDAG <46978506+KelsieTelson-SANDAG@users.noreply.github.com>
2020-08-11 11:23:29 -0700

    Create mohub_smartgrowth.R

---
+++ 2020-06 SCS Regional Forecast V2/R/mohub_smartgrowth.R
@@ -0,0 +1,48 @@
+
+
+#set up
+maindir = dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(maindir)
+
+source("readSQL.R")
+source("common_functions.R")
+
+packages <- c("RODBC","tidyverse","openxlsx","hash","readtext")
+pkgTest(packages)
+
+
+# connect to database
+channel <- odbcDriverConnect('driver={SQL Server}; server=sql2014a8; database=demographic_warehouse; trusted_connection=true')
+
+#pull xref table. Note: Nick acknowledges 38 MGRAS missing from this table that appear in 
+#[ws].[dbo].[MGRA13_in_MoHub_Amoeba_Edits_V5] table, but states these missing mgras have no capacity.
+xref<- data.table::as.data.table(
+  RODBC::sqlQuery(channel,
+                  paste0("SELECT [mgra]
+                  ,[mohub]
+                  ,[special_cap]
+                         FROM [urbansim].[ref].[scs_mgra_xref]
+                         WHERE [mohub_version_id] = 2"),
+                  stringsAsFactors = FALSE),
+  stringsAsFactors = FALSE)
+
+#retrieve mohub list used by Grace
+ws_mohubs<- data.table::as.data.table(
+  RODBC::sqlQuery(channel,
+                  paste0("SELECT [MGRA]
+      ,[MoHubName]
+      ,[MoHubType]
+  FROM [ws].[dbo].[MGRA13_in_MoHub_Amoeba_Edits_V5]"),
+                  stringsAsFactors = FALSE),
+  stringsAsFactors = FALSE)
+
+#combine tables to get full list of mgras in mobility hubs or smartgrowth areas
+mgra_list<-merge(xref,
+                 ws_mohubs,
+                 by.x="mgra",
+                 by.y="MGRA")
+
+#clean up
+odbcClose(channel)
+rm(xref,ws_mohubs,maindir,packages,channel)
+
