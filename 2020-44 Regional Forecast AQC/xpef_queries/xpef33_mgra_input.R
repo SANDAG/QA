6a0a08a969da3b146f8abd06007ea54a4c1c4d92
KelsieTelson-SANDAG <46978506+KelsieTelson-SANDAG@users.noreply.github.com>
2021-01-04 12:38:22 -0800

    updated xpef scripts for pbi

--- 2020-44 Regional Forecast AQC/xpef_queries/xpef33_mgra_input.R
+++ 2020-44 Regional Forecast AQC/xpef_queries/xpef33_mgra_input.R
@@ -10,69 +10,172 @@ pkgTest(packages)
 
 
 # connect to database
-channel <- odbcDriverConnect('driver={SQL Server}; server=sql2014a8; database=demographic_warehouse; trusted_connection=true')
+channel <- odbcDriverConnect('driver={SQL Server}; server=DDAMWSQL16; database=demographic_warehouse; trusted_connection=true')
 
 
 
 mgra_input<- data.table::as.data.table(
   RODBC::sqlQuery(channel,
                   paste0("SELECT [yr]
-                  ,[mgra]
-      ,sum([emp_ag]) as [emp_ag]
-      ,sum([emp_const_non_bldg_prod]) as [emp_const_non_bldg_prod] 
-      ,sum([emp_const_non_bldg_office]) as [emp_const_non_bldg_office]
-      ,sum([emp_utilities_prod]) as [emp_utilities_prod]
-      ,sum([emp_utilities_office]) as [emp_utilities_office]
-      ,sum([emp_const_bldg_prod]) as [emp_const_bldg_prod]
-      ,sum([emp_const_bldg_office]) as [emp_const_bldg_office]
-      ,sum([emp_mfg_prod]) as [emp_mfg_prod]
-      ,sum([emp_mfg_office]) as [emp_mfg_office]
-      ,sum([emp_whsle_whs]) as [emp_whsle_whs]
-      ,sum([emp_trans]) as [emp_trans]
-      ,sum([emp_retail]) as [emp_retail]
-      ,sum([emp_prof_bus_svcs]) as [emp_prof_bus_svcs]
-      ,sum([emp_prof_bus_svcs_bldg_maint]) as [emp_prof_bus_svcs_bldg_maint]
-      ,sum([emp_pvt_ed_k12]) as [emp_pvt_ed_k12]
-      ,sum([emp_pvt_ed_post_k12_oth]) as [emp_pvt_ed_post_k12_oth]
-      ,sum([emp_health]) as [emp_health] 
-      ,sum([emp_personal_svcs_office]) as [emp_personal_svcs_office]
-      ,sum([emp_amusement]) as [emp_amusement]
-      ,sum([emp_hotel]) as [emp_hotel]
-      ,sum([emp_restaurant_bar]) as [emp_restaurant_bar]
-      ,sum([emp_personal_svcs_retail]) as [emp_personal_svcs_retail]
-      ,sum([emp_religious]) as [emp_religious]
-      ,sum([emp_pvt_hh]) as [emp_pvt_hh]
-      ,sum([emp_state_local_gov_ent]) as [emp_state_local_gov_ent]
-      ,sum([emp_fed_non_mil]) as [emp_fed_non_mil]
-      ,sum([emp_fed_mil]) as [emp_fed_mil]
-      ,sum([emp_state_local_gov_blue]) as [emp_state_local_gov_blue]
-      ,sum([emp_state_local_gov_white]) as [emp_state_local_gov_white]
-      ,sum([emp_public_ed]) as [emp_public_ed]
-      ,sum([emp_own_occ_dwell_mgmt]) as [emp_own_occ_dwell_mgmt]
-      ,sum([emp_fed_gov_accts]) as [emp_fed_gov_accts]
-      ,sum([emp_st_lcl_gov_accts]) as [emp_st_lcl_gov_accts]
-      ,sum([emp_cap_accts]) as [emp_cap_accts]
-      ,sum([emp_total]) as [emp_total]
-      ,sum([enrollgradekto8]) as [enrollgradekto8]
-      ,sum([enrollgrade9to12]) as [enrollgrade9to12]
-      ,sum([collegeenroll]) as [collegeenroll]
-      ,sum([othercollegeenroll]) as [othercollegeenroll]
-      ,sum([adultschenrl]) as [adultschenrl]
-      ,sum([hstallsoth]) as [hstallsoth]
-      ,sum([hstallssam]) as [hstallssam]
-      ,avg([hparkcost]) as [hparkcost]
-      ,sum([dstallsoth]) as [dstallsoth]
-      ,sum([dstallssam]) as [dstallssam]
-      ,avg([dparkcost]) as [dparkcost]
-      ,sum([mstallsoth]) as [mstallsoth]
-      ,sum([mstallssam]) as [mstallssam]
-      ,avg([mparkcost]) as [mparkcost]
-      ,sum([hotelroomtotal]) as hotel_total
+      ,[mgra]
+      ,[emp_ag]
+      ,[emp_const_non_bldg_prod]
+      ,[emp_const_non_bldg_office]
+      ,[emp_utilities_prod]
+      ,[emp_utilities_office]
+      ,[emp_const_bldg_prod]
+      ,[emp_const_bldg_office]
+      ,[emp_mfg_prod]
+      ,[emp_mfg_office]
+      ,[emp_whsle_whs]
+      ,[emp_trans]
+      ,[emp_retail]
+      ,[emp_prof_bus_svcs]
+      ,[emp_prof_bus_svcs_bldg_maint]
+      ,[emp_pvt_ed_k12]
+      ,[emp_pvt_ed_post_k12_oth]
+      ,[emp_health]
+      ,[emp_personal_svcs_office]
+      ,[emp_amusement]
+      ,[emp_hotel]
+      ,[emp_restaurant_bar]
+      ,[emp_personal_svcs_retail]
+      ,[emp_religious]
+      ,[emp_pvt_hh]
+      ,[emp_state_local_gov_ent]
+      ,[emp_fed_non_mil]
+      ,[emp_fed_mil]
+      ,[emp_state_local_gov_blue]
+      ,[emp_state_local_gov_white]
+      ,[emp_public_ed]
+      ,[emp_own_occ_dwell_mgmt]
+      ,[emp_fed_gov_accts]
+      ,[emp_st_lcl_gov_accts]
+      ,[emp_cap_accts]
+      ,[emp_total]
+      ,[enrollgradekto8]
+      ,[enrollgrade9to12]
+      ,[collegeenroll]
+      ,[othercollegeenroll]
+      ,[adultschenrl]
+      ,[hstallsoth]
+      ,[hstallssam]
+      ,[hparkcost]
+      ,[numfreehrs]
+      ,[dstallsoth]
+      ,[dstallssam]
+      ,[dparkcost]
+      ,[mstallsoth]
+      ,[mstallssam]
+      ,[mparkcost]
+      ,[hotelroomtotal]
   FROM [isam].[xpef33].[abm_mgra13_based_input_np]
   GROUP BY [yr]
   ,[mgra]
+  ,[emp_ag]
+      ,[emp_const_non_bldg_prod]
+      ,[emp_const_non_bldg_office]
+      ,[emp_utilities_prod]
+      ,[emp_utilities_office]
+      ,[emp_const_bldg_prod]
+      ,[emp_const_bldg_office]
+      ,[emp_mfg_prod]
+      ,[emp_mfg_office]
+      ,[emp_whsle_whs]
+      ,[emp_trans]
+      ,[emp_retail]
+      ,[emp_prof_bus_svcs]
+      ,[emp_prof_bus_svcs_bldg_maint]
+      ,[emp_pvt_ed_k12]
+      ,[emp_pvt_ed_post_k12_oth]
+      ,[emp_health]
+      ,[emp_personal_svcs_office]
+      ,[emp_amusement]
+      ,[emp_hotel]
+      ,[emp_restaurant_bar]
+      ,[emp_personal_svcs_retail]
+      ,[emp_religious]
+      ,[emp_pvt_hh]
+      ,[emp_state_local_gov_ent]
+      ,[emp_fed_non_mil]
+      ,[emp_fed_mil]
+      ,[emp_state_local_gov_blue]
+      ,[emp_state_local_gov_white]
+      ,[emp_public_ed]
+      ,[emp_own_occ_dwell_mgmt]
+      ,[emp_fed_gov_accts]
+      ,[emp_st_lcl_gov_accts]
+      ,[emp_cap_accts]
+      ,[emp_total]
+      ,[enrollgradekto8]
+      ,[enrollgrade9to12]
+      ,[collegeenroll]
+      ,[othercollegeenroll]
+      ,[adultschenrl]
+      ,[hstallsoth]
+      ,[hstallssam]
+      ,[hparkcost]
+      ,[numfreehrs]
+      ,[dstallsoth]
+      ,[dstallssam]
+      ,[dparkcost]
+      ,[mstallsoth]
+      ,[mstallssam]
+      ,[mparkcost]
+      ,[hotelroomtotal]
   ORDER BY [yr]
-                         ,[mgra]"),
+                         ,[mgra]
+                         ,[emp_ag]
+      ,[emp_const_non_bldg_prod]
+      ,[emp_const_non_bldg_office]
+      ,[emp_utilities_prod]
+      ,[emp_utilities_office]
+      ,[emp_const_bldg_prod]
+      ,[emp_const_bldg_office]
+      ,[emp_mfg_prod]
+      ,[emp_mfg_office]
+      ,[emp_whsle_whs]
+      ,[emp_trans]
+      ,[emp_retail]
+      ,[emp_prof_bus_svcs]
+      ,[emp_prof_bus_svcs_bldg_maint]
+      ,[emp_pvt_ed_k12]
+      ,[emp_pvt_ed_post_k12_oth]
+      ,[emp_health]
+      ,[emp_personal_svcs_office]
+      ,[emp_amusement]
+      ,[emp_hotel]
+      ,[emp_restaurant_bar]
+      ,[emp_personal_svcs_retail]
+      ,[emp_religious]
+      ,[emp_pvt_hh]
+      ,[emp_state_local_gov_ent]
+      ,[emp_fed_non_mil]
+      ,[emp_fed_mil]
+      ,[emp_state_local_gov_blue]
+      ,[emp_state_local_gov_white]
+      ,[emp_public_ed]
+      ,[emp_own_occ_dwell_mgmt]
+      ,[emp_fed_gov_accts]
+      ,[emp_st_lcl_gov_accts]
+      ,[emp_cap_accts]
+      ,[emp_total]
+      ,[enrollgradekto8]
+      ,[enrollgrade9to12]
+      ,[collegeenroll]
+      ,[othercollegeenroll]
+      ,[adultschenrl]
+      ,[hstallsoth]
+      ,[hstallssam]
+      ,[hparkcost]
+      ,[numfreehrs]
+      ,[dstallsoth]
+      ,[dstallssam]
+      ,[dparkcost]
+      ,[mstallsoth]
+      ,[mstallssam]
+      ,[mparkcost]
+      ,[hotelroomtotal]"),
                   stringsAsFactors = FALSE))
 
 
@@ -110,10 +213,58 @@ y<- merge(mgra_input,
 
 y$jurisdiction<- ifelse(is.na(y$jurisdiction_2), y$jurisdiction_1, "Mixed")
 
-#final<- y[,list(
-#  pstudent=sum(pstudent),
-#  pop=sum(pop)),
-#  by=c("jurisdiction", "yr", "rac1p","ptype","pstudent", "pemploy","age_rc")]
+final<- y[,list(
+  emp_ag=sum(emp_ag),
+  emp_const_non_bldg_prod=sum(emp_const_non_bldg_prod),
+  emp_const_non_bldg_office=sum(emp_const_non_bldg_office),
+  emp_utilities_prod=sum(emp_utilities_prod),
+  emp_utilities_office=sum(emp_utilities_office),
+  emp_const_bldg_prod=sum(emp_const_bldg_prod),
+  emp_const_bldg_office=sum(emp_const_bldg_office),
+  emp_mfg_prod=sum(emp_mfg_prod),
+  emp_mfg_office=sum(emp_mfg_office),
+  emp_whsle_whs=sum(emp_whsle_whs),
+  emp_trans=sum(emp_trans),
+  emp_retail=sum(emp_retail),
+  emp_prof_bus_svcs=sum(emp_prof_bus_svcs),
+  emp_prof_bus_svcs_bldg_maint=sum(emp_prof_bus_svcs_bldg_maint),
+  emp_pvt_ed_k12=sum(emp_pvt_ed_k12),
+  emp_pvt_ed_post_k12_oth=sum(emp_pvt_ed_post_k12_oth),
+  emp_health=sum(emp_health), 
+  emp_personal_svcs_office=sum(emp_personal_svcs_office),
+  emp_amusement=sum(emp_amusement),
+  emp_hotel=sum(emp_hotel),
+  emp_restaurant_bar=sum(emp_restaurant_bar),
+  emp_personal_svcs_retail=sum(emp_personal_svcs_retail),
+  emp_religious=sum(emp_religious),
+  emp_pvt_hh=sum(emp_pvt_hh),
+  emp_state_local_gov_ent=sum(emp_state_local_gov_ent),
+  emp_fed_non_mil=sum(emp_fed_non_mil),
+  emp_fed_mil=sum(emp_fed_mil),
+  emp_state_local_gov_blue=sum(emp_state_local_gov_blue),
+  emp_state_local_gov_white=sum(emp_state_local_gov_white),
+  emp_public_ed=sum(emp_public_ed),
+  emp_own_occ_dwell_mgmt=sum(emp_own_occ_dwell_mgmt),
+  emp_fed_gov_accts=sum(emp_fed_gov_accts),
+  emp_st_lcl_gov_accts=sum(emp_st_lcl_gov_accts),
+  emp_cap_accts=sum(emp_cap_accts),
+  emp_total=sum(emp_total),
+  enrollgradekto8=sum(enrollgradekto8),
+  enrollgrade9to12=sum(enrollgrade9to12),
+  collegeenroll=sum(collegeenroll),
+  othercollegeenroll=sum(othercollegeenroll),
+  adultschenrl=sum(adultschenrl),
+  hstallsoth=sum(hstallsoth),
+  hstallssam=sum(hstallssam),
+  hparkcost=mean(hparkcost),
+  dstallsoth=sum(dstallsoth),
+  dstallssam=sum(dstallssam),
+  dparkcost=mean(dparkcost),
+  mstallsoth=sum(mstallsoth),
+  mstallssam=sum(mstallssam),
+  mparkcost=mean(mparkcost),
+  hotel_total=sum(hotelroomtotal)),
+  by=c("jurisdiction", "yr")]
 
 #saveout merged table
-write.csv(y, "C://Users//kte//San Diego Association of Governments//SANDAG QA QC - Documents//Projects//2020//2020-44 Regional Forecast AQC//Results//PowerBI//mgra_input_jur.csv")
+write.csv(final, "C://Users//kte//San Diego Association of Governments//SANDAG QA QC - Documents//Projects//2020//2020-44 Regional Forecast AQC//Results//PowerBI//mgra_input_jur.csv")
