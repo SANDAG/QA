a134d8232a3a8eb1691c5ff72a791b86b53638cd
calvinraab <calvin.raab@sandag.org>
2023-03-16 13:43:22 -0700

    LUZ information update

--- 2023/2023-024 Estimates 2022/series_15_estimates_automation/SQL_Queries/household_type_query.sql
+++ 2023/2023-024 Estimates 2022/series_15_estimates_automation/SQL_Queries/household_type_query.sql
@@ -1,8 +1,15 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
+-- SELECT [mgra_id]
+--       ,[yr_id]
+--       ,household_type_id.name AS 'household_type'
+--       ,[households]
+--   FROM [estimates].[est_2022_01].[dw_households] AS household_table
+--   LEFT JOIN [demographic_warehouse].[dim].[household_type] AS household_type_id
+--   ON household_table.household_size_id = household_type_id.household_type_id
+
+
 SELECT [mgra_id]
-      ,[yr_id]
-      ,household_type_id.name AS 'household_type'
-      ,[households]
-  FROM [estimates].[est_2022_01].[dw_households] AS household_table
-  LEFT JOIN [demographic_warehouse].[dim].[household_type] AS household_type_id
-  ON household_table.household_size_id = household_type_id.household_type_id
+    ,[yr_id]
+    ,household_size_id
+    ,[households]
+    FROM [estimates].[est_2022_01].[dw_households] AS household_table
