e49d2038d1e70c11bc97d3ce473bb7ad0dc23064
calvinraab <calvin.raab@sandag.org>
2023-03-27 12:12:02 -0700

    Building Scalable Automation

---
+++ 2023/2023-023 Estimates 2022/series_15_estimates_automation_2/sql_queries/age_query.sql
@@ -0,0 +1,11 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP(100) series_15_denorm.[{geo_level}] AS 'geo_level'
+      ,[yr_id]
+	  ,age_group_table.name AS 'breakdown_value'
+      ,SUM([population]) AS 'value'
+  FROM [estimates].[est_{estimates_version}].[dw_age] AS age_table
+  LEFT JOIN [demographic_warehouse].[dim].[age_group] AS age_group_table
+  ON age_table.age_group_id = age_group_table.age_group_id
+  LEFT JOIN [ws].[dbo].[series_15_mgra_denorm] AS series_15_denorm
+  ON age_table.mgra_id = series_15_denorm.mgra_id
+  GROUP BY series_15_denorm.[{geo_level}], yr_id, age_group_table.name
